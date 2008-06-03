5112b8bd2d9b910602aaeb5827fb64dfdbf6d2ff
paus <>
2008-06-03 07:21:28 +0000

    Minor cleanup and adding examples (see MitSoft TWiki).

--- macros/examples/runSimpleExample.C
+++ macros/examples/runSimpleExample.C
@@ -1,4 +1,4 @@
-// $Id: runAna.C 4148 2007-06-21 12:22:37Z loizides $
+// $Id: runSimpleExample.C,v 1.1 2008/05/27 19:59:54 loizides Exp $
 
 #if !defined(__CINT__) || defined(__MAKECINT__)
 #include "MitAna/DataUtil/interface/Debug.h"
@@ -6,31 +6,31 @@
 #include "MitAna/TreeMod/interface/Analysis.h"
 #endif
 
-
-//________________________________________________________________________
-void runSimpleExample(const char *files, const char *tname="MitTree")
+//--------------------------------------------------------------------------------------------------
+void runSimpleExample(const char *files = "mit-gen_000.root", const char *tname = "MitTree")
 {
   using namespace mithep;
-  gDebugMask = Debug::kAnalysis;
-  gDebugLevel = 1; 
+  gDebugMask  = Debug::kAnalysis;
+  gDebugLevel = 1;
 
   ParticleExampleMod *mod = new ParticleExampleMod;
-  //mod->
 
-  // set up top-level analysis class
+  // set up analysis
   Analysis *ana = new Analysis;
   ana->SetTreeName(tname);
   ana->SetSuperModule(mod);
   ana->AddFile(files);
-  ana->SetOutputName("ana_testoutput.root"); 
+  ana->SetOutputName("mit-gen-hist.root");
 
-  if(ana->Init()) {
+  // run the analysis after successful initialisation
+  if (ana->Init()) {
     ana->Run();
     ana->Terminate();
   } else {
     ::Error("runAna", "Could not initialize analysis.");
-  } 
+  }
 
+  // cleanup ( someone is really worried here :-) )
   delete ana;
   delete mod;
 }
--- macros/rootlogon.C
+++ macros/rootlogon.C
@@ -1,25 +1,13 @@
-// $Id: rootlogon.C 2637 2006-03-06 23:53:38Z loizides $
+// $Id: rootlogon.C,v 1.1 2008/05/27 19:59:54 loizides Exp $
 
 {
-  if(1) {
-    if(gSystem->Getenv("CMSSW_VERSION")) {
-      if(gSystem->Load("libFWCoreFWLite.so")==0) {
-        AutoLibraryLoader::enable();
-        //AutoLibraryLoader::loadAll();
-
-        ::Info("rootlogon.C", 
-               "Setting up for %s", gSystem->Getenv("CMSSW_VERSION"));
-      } 
-
-      if(1) {
-        TString str = gSystem->GetMakeSharedLib();
-        if(str.Contains("-m32")==0 && str.Contains("-m64")==0) {
-          str.ReplaceAll("g++", "g++ -m32");
-          gSystem->SetMakeSharedLib(str);
-        }
-      }
+  if (gSystem->Getenv("CMSSW_VERSION")) {
+    TString str = gSystem->GetMakeSharedLib();
+    if (str.Contains("-m32")==0 && str.Contains("-m64")==0) {
+      str.ReplaceAll("g++", "g++ -m32");
+      gSystem->SetMakeSharedLib(str);
     }
   }
-
+  
   gROOT->Macro("$CMSSW_BASE/src/MitAna/macros/setRootEnv.C+");
 }
