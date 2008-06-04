bd784c20fc6d43b752c8886cbeae113568af67a4
loizides <>
2008-06-04 20:00:14 +0000

    Hack is not needed anymore since we do not derive from FourVector but include it in the Particle.

--- DataTree/interface/Types.h
+++ DataTree/interface/Types.h
@@ -1,4 +1,4 @@
-// $Id: Types.h,v 1.2 2008/05/27 19:59:54 loizides Exp $
+// $Id: Types.h,v 1.3 2008/06/01 10:03:39 loizides Exp $
 
 #ifndef DATATREE_TYPES_H
 #define DATATREE_TYPES_H
@@ -6,18 +6,9 @@
 #include <Rtypes.h>
 #include <Math/GenVector/LorentzVector.h>
 
-typedef ::ROOT::Math::LorentzVector< ::ROOT::Math::PxPyPzE4D<double> > FourVector;
-
-/* this is a hack needed for current version of rootcint */
 namespace mithep 
 {
-  namespace ROOT 
-  {
-    namespace Math 
-    {
-      using namespace ::ROOT::Math;
-    }
-  }
-} /*namespace mithep*/
+  typedef ::ROOT::Math::LorentzVector< ::ROOT::Math::PxPyPzE4D<double> > FourVector;
+}
 
 #endif /*DATATREE_TYPES_H*/
