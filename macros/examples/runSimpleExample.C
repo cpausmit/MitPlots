ef842b921cc08bffbc564fef0bebb8caf0ab83bb
loizides <>
2008-06-05 09:46:57 +0000

    Simplified macro

--- macros/examples/runSimpleExample.C
+++ macros/examples/runSimpleExample.C
@@ -1,36 +1,28 @@
-// $Id: runSimpleExample.C,v 1.1 2008/05/27 19:59:54 loizides Exp $
+// $Id: runSimpleExample.C,v 1.2 2008/06/03 07:21:28 paus Exp $
 
 #if !defined(__CINT__) || defined(__MAKECINT__)
+#include <TROOT.h>
 #include "MitAna/DataUtil/interface/Debug.h"
 #include "MitAna/TreeMod/interface/ParticleExampleMod.h"
 #include "MitAna/TreeMod/interface/Analysis.h"
 #endif
 
 //--------------------------------------------------------------------------------------------------
-void runSimpleExample(const char *files = "mit-gen_000.root", const char *tname = "MitTree")
+void runSimpleExample(const char *files = "mit-gen_000.root")
 {
   using namespace mithep;
   gDebugMask  = Debug::kAnalysis;
   gDebugLevel = 1;
 
+  // set up the modules
   ParticleExampleMod *mod = new ParticleExampleMod;
 
   // set up analysis
   Analysis *ana = new Analysis;
-  ana->SetTreeName(tname);
   ana->SetSuperModule(mod);
   ana->AddFile(files);
   ana->SetOutputName("mit-gen-hist.root");
 
   // run the analysis after successful initialisation
-  if (ana->Init()) {
-    ana->Run();
-    ana->Terminate();
-  } else {
-    ::Error("runAna", "Could not initialize analysis.");
-  }
-
-  // cleanup ( someone is really worried here :-) )
-  delete ana;
-  delete mod;
+  ana->Run(!gROOT->IsBatch());
 }
