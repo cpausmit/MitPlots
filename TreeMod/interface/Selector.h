17acbfeefb7bdc3d9e992660cd105fdf41cac025
loizides <>
2008-06-24 14:08:39 +0000

    Deal with run info information

--- TreeMod/interface/Selector.h
+++ TreeMod/interface/Selector.h
@@ -1,11 +1,11 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: BaseMod.h,v 1.2 2008/06/18 19:08:14 loizides Exp $
+// $Id: Selector.h,v 1.1 2008/06/23 10:54:03 loizides Exp $
 //
-// Selector
-//
+// Our selector class for modular processing of a tree (or chain). In addition to the generic
+// TAMSelector it determines the begin and end of a run and does the necessary bookkeeping. 
+// Modules can ask the selector to provide the run information. (This needs to be implemented.)
 //
 // Authors: C.Loizides
-//
 //--------------------------------------------------------------------------------------------------
 
 #ifndef TREEMOD_SELECTOR_H
@@ -13,6 +13,9 @@
 
 #include "MitAna/TAM/interface/TAModule.h" 
 #include "MitAna/TAM/interface/TAMSelector.h" 
+#include "MitAna/DataTree/interface/EventHeader.h" 
+#include "MitAna/DataTree/interface/LAHeader.h" 
+#include "MitAna/DataTree/interface/RunInfo.h" 
 
 namespace mithep {
   class Selector : public TAMSelector
@@ -21,14 +24,39 @@ namespace mithep {
       Selector();
       ~Selector();
    
-      //void              Init(TTree* tree);
-      //Bool_t            Notify();
-      //Bool_t            Process(Long64_t entry);
-      //void              SlaveBegin(TTree* tree);
-      //void              SlaveTerminate();
-      // void              Terminate();
-   
+      Bool_t            BeginRun();
+      Bool_t            EndRun();
+      Bool_t            Notify();
+      void              SlaveBegin(TTree* tree);
+      Bool_t            ValidRunInfo()           const;
+      Bool_t            ValidRunNum()            const { return fCurRunNum!=UInt_t(-1); }
+
+    protected:
+      Bool_t            ConsistentRunNum()       const;
+      void              UpdateRunInfo();
+      void              UpdateRunInfoTree();
+
+      Bool_t            fDoRunInfo;      //=true then get RunInfo (def=1)
+      TTree            *fRunTree;        //run info tree in current file
+      EventHeader      *fEventHeader;    //event header for current event
+      RunInfo          *fRunInfo;        //run information for current run
+      TTree            *fLATree;         //look-ahead tree in current file
+      LAHeader         *fLAHeader;       //event header for next event
+      UInt_t            fCurRunNum;      //current run number
+
       ClassDef(Selector,1)
   };
+
+//--------------------------------------------------------------------------------------------------
+  inline Bool_t Selector::ConsistentRunNum() const
+  {
+    return (ValidRunNum() && fCurRunNum==fEventHeader->RunNum());
+  }
+
+//--------------------------------------------------------------------------------------------------
+  inline Bool_t Selector::ValidRunInfo() const
+  {
+    return (fRunInfo && fCurRunNum==fRunInfo->RunNum());
+  }
 }
 #endif
--- TreeMod/src/Selector.cc
+++ TreeMod/src/Selector.cc
@@ -1,15 +1,174 @@
-// $Id: BaseMod.cc,v 1.1 2008/06/12 08:47:57 loizides Exp $
+// $Id: Selector.cc,v 1.1 2008/06/23 10:54:03 loizides Exp $
 
 #include "MitAna/TreeMod/interface/Selector.h"
+#include "MitAna/DataTree/interface/Names.h"
+
+#include <TProcessID.h>
+#include <TFile.h>
+#include <TTree.h>
+
 
 using namespace mithep;
 
 ClassImp(mithep::Selector)
 
-Selector::Selector()
+//__________________________________________________________________________________________________
+Selector::Selector() :
+  fDoRunInfo(kTRUE),
+  fRunTree(0),
+  fEventHeader(0),
+  fRunInfo(0),
+  fCurRunNum(UInt_t(-1)),
+  fLATree(0),
+  fLAHeader(0)
 {
 }
 
+//__________________________________________________________________________________________________
 Selector::~Selector()
 {
+}
+
+//__________________________________________________________________________________________________
+Bool_t Selector::BeginRun()
+{
+  // Determines whether we are at beginning of a new run.
+
+  if (!fDoRunInfo) 
+    return kFALSE;
+
+  LoadBranch(Names::gkEvtHeaderBrn);
+  if (!fEventHeader) 
+    return kFALSE;
+
+  if (ConsistentRunNum())
+    return kFALSE;
+
+  UpdateRunInfo();
+  return ValidRunNum();
+}
+
+//__________________________________________________________________________________________________
+Bool_t Selector::EndRun()
+{
+  // Determines whether we are at the end of a run.
+
+  if (!fDoRunInfo) 
+    return kFALSE;
+
+  if (!ValidRunNum())
+    return kFALSE;
+
+  // determine if run will end
+  if (fCurEvt+1==fTree->GetTree()->GetEntries())
+    return kTRUE; // we are at last entry in current file
+
+  fLAHeader=0;
+  Int_t ret = fLATree->GetEvent(fCurEvt);
+  if (ret<0 || fLAHeader==0) {
+    Error("EndRun", "Could not get entry lookahead entry for next event,"
+                    " assuming end of run %ld reached!", fCurRunNum);
+    return kTRUE;
+  }
+
+  return (fLAHeader->RunNum()!=fCurRunNum);
+}
+
+//__________________________________________________________________________________________________
+Bool_t Selector::Notify()
+{
+  // The Notify() function is called when a new file is opened. Here, we check for a new run info
+  // tree. 
+
+  if (!GetCurrentFile()) 
+    return kTRUE;
+
+  if (fDoRunInfo) 
+    UpdateRunInfoTree();
+
+  return TAMSelector::Notify();
+}
+
+//__________________________________________________________________________________________________
+void Selector::SlaveBegin(TTree* tree)
+{
+  // The SlaveBegin() function is called after the Begin() function and can be used to setup
+  // analysis on the slaves. Here, we request the event header branch.
+
+  if (fDoRunInfo) {
+    ReqBranch(Names::gkEvtHeaderBrn, fEventHeader);
+  }
+
+  TAMSelector::SlaveBegin(tree);
+}
+
+//__________________________________________________________________________________________________
+void Selector::UpdateRunInfo() 
+{
+  // Update the run info to be consistent with the information from the event header.
+
+  fRunInfo   = 0;
+
+  if (!fRunTree) 
+    return;
+
+  Int_t ret = fRunTree->GetEvent(fEventHeader->RunEntry());
+  if (ret<0 || fRunInfo==0) {
+    Error("UpdateRunInfo", "Error updating run info for run %ld, entry %ld, return value %d", 
+          fEventHeader->RunNum(), fEventHeader->RunEntry(), ret);
+    return;
+  }
+
+  fCurRunNum = fEventHeader->RunNum();
+  if (!ValidRunInfo()) {
+    Error("UpdateRunInfo", "Error updating run info, run values do not match %d %d", 
+          fCurRunNum, fRunInfo->RunNum());
+    return;
+  }
+}
+
+//__________________________________________________________________________________________________
+void Selector::UpdateRunInfoTree() 
+{
+  // Get the run info tree from current file and set our branches.
+
+  // first erase previous entries
+  fRunTree   = 0;
+  fRunInfo   = 0;
+  fCurRunNum = UInt_t(-1);
+  fLATree    = 0;
+  fLAHeader  = 0;
+
+  // get current file and retrieve trees
+  TFile *f = GetCurrentFile();
+  if (!f) {
+    Error("UpdateRunInfoTree", "Ptr to current file is null");
+    return;
+  }
+
+  // run info tree
+  fRunTree = dynamic_cast<TTree*>(f->Get(Names::gkRunTreeName));
+  if (!fRunTree) {
+    Fatal("UpdateRunInfoTree", "Can not find run info tree with name %s", Names::gkRunTreeName);
+  }
+
+  // set branches 
+  if (fRunTree->GetBranch(Names::gkRunInfoBrn)) {
+    fRunTree->SetBranchAddress(Names::gkRunInfoBrn, &fRunInfo);
+  } else {
+    Fatal("UpdateRunInfoTree", "Can not find run info branch with name %s", Names::gkRunInfoBrn);
+  }
+
+  // look-ahead tree
+  fLATree = dynamic_cast<TTree*>(f->Get(Names::gkLATreeName));
+  if (!fLATree) {
+    Fatal("UpdateRunInfoTree", "Can not find look-ahead tree with name %s", Names::gkLATreeName);
+  }
+
+  // set branches 
+  if (fLATree->GetBranch(Names::gkLAHeaderBrn)) {
+    fLATree->SetBranchAddress(Names::gkLAHeaderBrn, &fLAHeader);
+  } else {
+    Fatal("UpdateRunInfoTree", "Can not find look-ahead branch with name %s", Names::gkLAHeaderBrn);
+  }
 }
