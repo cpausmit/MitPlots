26e747f426ff7bfde2692f0440c60fe4b95f3165
loizides <>
2008-07-01 09:17:11 +0000

    SimPartice has simparticle daughers

--- DataTree/interface/SimParticle.h
+++ DataTree/interface/SimParticle.h
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: GenParticle.h,v 1.5 2008/06/18 19:08:14 loizides Exp $
+// $Id: SimParticle.h,v 1.1 2008/06/30 16:51:48 loizides Exp $
 //
 // SimParticle
 //
@@ -19,18 +19,36 @@ namespace mithep
   class SimParticle : public GenParticle
   {
     public:
-      SimParticle() : fGenPartRef(0) {}
+      SimParticle() {}
       SimParticle(Double_t px, Double_t py, Double_t pz, Double_t e, Int_t id) 
         : GenParticle(px,py,pz,e,id,0) {}
       ~SimParticle() {}
       
-      void 		SetGenParticle(GenParticle* p) { fGenPartRef = p; }
-      GenParticle*	GetGenParticle() const { return (GenParticle*)fGenPartRef.GetObject(); }
+      const SimParticle   *GetDaughter(UInt_t i) const;
+      const GenParticle*   GetGenParticle()      const;
+      Bool_t               IsGenerated()         const { return fGenPartRef.IsValid(); }
+      void                 SetGenParticle(GenParticle* p) { fGenPartRef = p; }
       
     protected:
-      TRef		fGenPartRef;
+      TRef		   fGenPartRef; //reference to generated particle
 
     ClassDef(SimParticle,1) // Simulated particle class
   };
+}
+
+//--------------------------------------------------------------------------------------------------
+inline const mithep::SimParticle *mithep::SimParticle::GetDaughter(UInt_t i) const 
+{ 
+  // Return daughter corresponding to given index.
+
+  return static_cast<const SimParticle*>(fDaughters.At(i)); 
+}
+
+//--------------------------------------------------------------------------------------------------
+inline const mithep::GenParticle *mithep::SimParticle::GetGenParticle() const 
+{ 
+  // Return reference to generated particle.
+
+  return static_cast<const GenParticle*>(fGenPartRef.GetObject()); 
 }
 #endif
