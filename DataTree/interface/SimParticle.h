6e5f3d7fac194b2fe5740f39abf3f69ab1628b50
loizides <>
2008-07-07 16:01:46 +0000

    Cosmetics.

--- DataTree/interface/SimParticle.h
+++ DataTree/interface/SimParticle.h
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: SimParticle.h,v 1.3 2008/07/01 14:09:27 loizides Exp $
+// $Id: SimParticle.h,v 1.4 2008/07/07 13:27:58 bendavid Exp $
 //
 // SimParticle
 //
@@ -27,7 +27,7 @@ namespace mithep
       const SimParticle   *GetDaughter(UInt_t i) const;
       const GenParticle*   GetGenParticle()      const;
       const SimParticle   *GetMother()           const;
-      Bool_t               IsGenerated()         const { return fGenPartRef.IsValid(); }
+      Bool_t               IsGenerated()         const    { return fGenPartRef.IsValid(); }
       void                 SetGenParticle(GenParticle* p) { fGenPartRef = p; }
 
     protected:
