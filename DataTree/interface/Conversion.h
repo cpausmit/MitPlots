7edacb7ccc18adcbe5e0273225d202cc87db0594
bendavid <>
2008-07-07 15:32:52 +0000

    added more conversion details

--- DataTree/interface/Conversion.h
+++ DataTree/interface/Conversion.h
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: Conversion.h,v 1.1 2008/07/02 19:28:43 bendavid Exp $
+// $Id: Conversion.h,v 1.2 2008/07/03 08:22:17 loizides Exp $
 //
 // Conversion
 //
@@ -27,11 +27,23 @@ namespace mithep
       ~Conversion() {}
       
       const Electron           *GetDaughter(UInt_t i) const;
+      Double_t                  GetDCotTheta()        const { return fDCotTheta; }
+      Double_t                  GetEOverP()           const { return fEOverP; }
+      Double_t                  GetPairMass()         const { return fPairMass; }
+      const ThreeVector        &GetPairMomentum()     const { return fPairMomentum; }
       const FitVertex          &GetVertex()           const { return fVertex; }
       FitVertex                &GetVertex()                 { return fVertex; }
+      void                      SetDCotTheta(Double_t dCotTheta) { fDCotTheta = dCotTheta;}
+      void                      SetEOverP(Double_t eOverP)       { fEOverP = eOverP;}
+      void                      SetPairMass(Double_t pairMass)   { fPairMass = pairMass;}
+      void                      SetPairMomentum(Double_t px, Double_t py, Double_t pz);
         
     protected:
       FitVertex                 fVertex; //reconstructed decay vertex
+      Double_t                  fDCotTheta; //delta cot theta between the tracks at the conversion point
+      Double_t                  fEOverP; //E over p of conversion tracks
+      Double_t                  fPairMass; //mass of electron-positron pair at conversion point
+      ThreeVector               fPairMomentum; //momentum of tracks at conversion point
       
     ClassDef(Conversion,1) // Conversion class
   };
@@ -43,5 +55,11 @@ inline const mithep::Electron *mithep::Conversion::GetDaughter(UInt_t i) const
   // Return reference to electron daughters.
 
   return static_cast<const mithep::Electron*>(fDaughters.At(i)); 
+}
+
+//--------------------------------------------------------------------------------------------------
+inline void mithep::Conversion::SetPairMomentum(Double_t px, Double_t py, Double_t pz)
+{ 
+  fPairMomentum.SetXYZ(px,py,pz);; 
 }
 #endif
