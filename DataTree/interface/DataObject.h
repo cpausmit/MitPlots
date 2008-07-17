21241b787445d997c30fbe2132aa1a95b1d97778
bendavid <>
2008-07-17 16:37:37 +0000

    Added ability to find parent collection of any data object

--- DataTree/interface/DataObject.h
+++ DataTree/interface/DataObject.h
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: DataObject.h,v 1.8 2008/07/13 08:28:33 loizides Exp $
+// $Id: DataObject.h,v 1.9 2008/07/14 20:55:19 loizides Exp $
 //
 // DataObject
 //
@@ -12,6 +12,11 @@
 #define DATATREE_DATAOBJECT_H
  
 #include <TObject.h>
+#include <TROOT.h>
+#include <TClass.h>
+#include <TBranchElement.h>
+#include <TRefTable.h>
+#include <TProcessID.h>
 #include "MitAna/DataTree/interface/Types.h"
 
 namespace mithep 
@@ -25,6 +30,7 @@ namespace mithep
       Bool_t               MustClear()   const { return TestBit(14); }
       Bool_t               MustDelete()  const { return TestBit(15); }
       Bool_t               IsCached()    const { return TestBit(23); }
+      template <class Col> Col* ParentCol();
 
     protected:
       void                 SetClearBit()       { SetBit(14); }
@@ -34,5 +40,28 @@ namespace mithep
 
     ClassDef(DataObject, 1) // Basic data object class
   };
+}
+
+//--------------------------------------------------------------------------------------------------
+template <class Col>
+Col* mithep::DataObject::ParentCol()
+{
+  // Return pointer to parent collection.  SLOW!
+  
+  Col* colObj=0;
+  TRefTable *table = TRefTable::GetRefTable();
+  if (!table)
+    return colObj;
+  table->SetUID(this->GetUniqueID(), (TProcessID*)gROOT->GetUUIDs());
+  table->Notify();
+  TBranchElement *trackParent = (TBranchElement*)(table->GetParent(this->GetUniqueID(), TProcessID::GetProcessWithUID(this->GetUniqueID(),this)));
+  while (!colObj) {
+    if (!trackParent)
+      return colObj;
+    colObj = dynamic_cast<Col*>((TObject*)((TBranchElement*)trackParent)->GetObject());
+    if (!colObj)
+      trackParent = dynamic_cast<TBranchElement*>(trackParent->GetMother());
+  }
+    return colObj;
 }
 #endif
