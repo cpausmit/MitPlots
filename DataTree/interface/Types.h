acb64ed9cdaa0fbb82d90210504d06fbbae65e3b
sixie <>
2008-08-18 10:58:20 +0000

    Added etaphi.

--- DataTree/interface/Types.h
+++ DataTree/interface/Types.h
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: Types.h,v 1.13 2008/07/29 22:51:56 bendavid Exp $
+// $Id: Types.h,v 1.14 2008/07/30 08:38:04 loizides Exp $
 //
 // Types
 //
@@ -19,10 +19,14 @@
 namespace mithep
 {
   typedef ::ROOT::Math::LorentzVector< ::ROOT::Math::PxPyPzE4D<double> > FourVector;
+  typedef ::ROOT::Math::LorentzVector< ::ROOT::Math::PtEtaPhiE4D<double> > FourVectorEtaPhi;
+
   typedef ::ROOT::Math::DisplacementVector3D<ROOT::Math::Cartesian3D<double>,
-                                             ROOT::Math::DefaultCoordinateSystemTag> ThreeVector;
+                                             ROOT::Math::DefaultCoordinateSystemTag> 
+                                             ThreeVector;
   typedef ::ROOT::Math::DisplacementVector3D<ROOT::Math::CylindricalEta3D<double>,
-                                             ROOT::Math::DefaultCoordinateSystemTag> ThreeVectorEtaPhi;
+                                             ROOT::Math::DefaultCoordinateSystemTag> 
+                                             ThreeVectorEtaPhi;
   typedef ::ROOT::Math::SMatrix<double,3,3,ROOT::Math::MatRepSym<double,3> >   ThreeSymMatrix;
   typedef ::ROOT::Math::SMatrix<double,7,7,ROOT::Math::MatRepSym<double,7> >   SevenSymMatrix;
   typedef ::ROOT::Math::SMatrix<double,3,3,ROOT::Math::MatRepStd<double,3,3> > ThreeMatrix;
