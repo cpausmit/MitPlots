08b526e030c4a7e16c18cbc50134b9b3f33c5a2a
sixie <>
2008-09-06 18:03:23 +0000

    Remove functions to calculate isolation for electrons. This functionality was moved to a separate class

--- DataTree/interface/Electron.h
+++ DataTree/interface/Electron.h
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: Electron.h,v 1.14 2008/08/18 10:56:03 sixie Exp $
+// $Id: Electron.h,v 1.15 2008/08/22 09:55:40 sixie Exp $
 //
 // Electron
 //
@@ -30,7 +30,6 @@ namespace mithep
       const Track         *Trk()                   const { return BestTrk();                }
       Double_t             E()                     const {return SCluster()->Energy();      } 
 
-
       Double_t    Mass()                           const { return 0.51099892e-3;            }
       Double_t    ESuperClusterOverP()             const { return fESuperClusterOverP;      }
       Double_t    ESeedClusterOverPout()           const { return fESeedClusterOverPout;    }
@@ -52,15 +51,11 @@ namespace mithep
       Double_t    CovEtaPhi()                      const { return fCovEtaPhi;               }
       Double_t    CovPhiPhi()                      const { return fCovPhiPhi;               }
       Double_t    CaloIsolation()                  const { return fCaloIsolation;           }
+      Double_t    CaloTowerIsolation()             const { return fCaloTowerIsolation;      }
       Double_t    TrackIsolation()                 const { return fTrackIsolation;          }
       Double_t    PassLooseID()                    const { return fPassLooseID;             }
       Double_t    PassTightID()                    const { return fPassTightID;             }
       Double_t    IDLikelihood()                   const { return fIDLikelihood;            }
-      Double_t    ComputeTrackIsolation   (  Double_t extRadius, Double_t intRadius, 
-                                             Double_t ptLow, Double_t maxVtxZDist, 
-                                             mithep::Collection<Track> *tracks              );
-      Double_t    ComputeEcalIsolation    (  Double_t coneSize, Double_t etLow, 
-                                             mithep::Collection<BasicCluster> *basicClusters);
 
       void	  SetGsfTrk(Track* t)                           { fGsfTrackRef = t;                }
       void	  SetTrackerTrk(Track* t)                       { fTrackerTrackRef = t;            }
@@ -84,6 +79,7 @@ namespace mithep
       void        SetCovEtaPhi(Double_t CovEtaPhi)              { fCovEtaPhi = CovEtaPhi;          }
       void        SetCovPhiPhi(Double_t CovPhiPhi)              { fCovPhiPhi = CovPhiPhi;          }
       void        SetCaloIsolation(Double_t CaloIsolation)      { fCaloIsolation = CaloIsolation;  }
+      void        SetCaloTowerIsolation(Double_t TowerIso)      { fCaloTowerIsolation = TowerIso;  }
       void        SetTrackIsolation(Double_t TrackIsolation)    { fTrackIsolation = TrackIsolation;}
       void        SetPassLooseID(Double_t passLooseID)          { fPassLooseID = passLooseID;      }
       void        SetPassTightID(Double_t passTightID)          { fPassTightID = passTightID;      }
@@ -115,6 +111,7 @@ namespace mithep
       Double_t             fCovEtaPhi;
       Double_t             fCovPhiPhi;
       Double_t             fCaloIsolation;
+      Double_t             fCaloTowerIsolation;
       Double_t             fTrackIsolation;
       Double_t             fPassLooseID;
       Double_t             fPassTightID;
