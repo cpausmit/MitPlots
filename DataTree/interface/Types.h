ecc6081830385a673d21eb07fe5481bbe67ff58d
loizides <>
2008-09-17 04:18:56 +0000

    Add BitMask types.

--- DataTree/interface/Types.h
+++ DataTree/interface/Types.h
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: Types.h,v 1.17 2008/09/05 01:21:45 loizides Exp $
+// $Id: Types.h,v 1.18 2008/09/10 03:33:27 loizides Exp $
 //
 // Types
 //
@@ -12,16 +12,18 @@
 #define MITANA_DATATREE_TYPES_H
  
 #include <Rtypes.h>
-#include <Math/GenVector/LorentzVector.h>
-#include <Math/Point3Dfwd.h>
-#include <Math/SMatrix.h>
 #include "MitCommon/DataFormats/interface/Types.h"
 #include "MitAna/DataTree/interface/BitMask.h"
 
 namespace mithep
 {
-  //typedef BitMask<4> BitMask32;
-  //typedef BitMask<8> BitMask64;
+  typedef BitMask<1>  BitMask8;
+  typedef BitMask<2>  BitMask16;
+  typedef BitMask<4>  BitMask32;
+  typedef BitMask<5>  BitMask40;
+  typedef BitMask<6>  BitMask48;
+  typedef BitMask<7>  BitMask56;
+  typedef BitMask<8>  BitMask64;
   typedef BitMask<32> BitMask256;
 }
 #endif
