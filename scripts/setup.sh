ff06d5b41ae0384c49f0dba80e7e2f9929a363b1
loizides <>
2008-09-27 06:15:18 +0000

    Cleanup.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# $Id: setup.sh,v 1.1 2008/09/17 05:08:21 loizides Exp $
+# $Id: setup.sh,v 1.2 2008/09/17 19:27:05 sixie Exp $
 
 if test -z $CMSSW_VERSION; then
     echo "Need cmssw project area setup!";
@@ -18,7 +18,7 @@ echo "Setup called for $CMSSW_VERSION ($version)";
 
 cd $CMSSW_BASE/src;
 
-if      test $version -lt 20108; then
+if test $version -lt 20108; then
     echo "Nothing to be done, exiting";
     exit 1;
 elif test $version -eq 20108; then
@@ -27,9 +27,14 @@ elif test $version -eq 20108; then
     cvs co -r V02-09-00 JetMETCorrections/Modules;
 
     #to remove annoying Warning messages for the jet to vertex associator.
-    cvs co JetMETCorrections/JetVertexAssociation
-    cat JetMETCorrections/JetVertexAssociation/src/JetVertexMain.cc | sed -e 's/else  std::cout << \"\[Jets\] JetVertexAssociation: Warning\! problems for  Algo = 2: possible division by zero ..\" << std::endl;//' > tmp
-    mv tmp JetMETCorrections/JetVertexAssociation/src/JetVertexMain.cc 
+    cvs co JetMETCorrections/JetVertexAssociation;
+    TMP=`mktemp`;
+    cat JetMETCorrections/JetVertexAssociation/src/JetVertexMain.cc | 
+    sed -e 's/else  std::cout << \"\[Jets\] JetVertexAssociation: Warning\! problems for  Algo = 2: possible division by zero ..\" << std::endl;//' > $TMP;
+    mv $TMP JetMETCorrections/JetVertexAssociation/src/JetVertexMain.cc
+else
+    echo "Nothing known about this version, exiting";
+    exit 1;
 fi
 
 echo "Setup done; you probably want to compile your project area now";
