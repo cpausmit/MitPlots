a0e5a7fd8a8d1ec5ecaaeb9649f0ab8af1b7e862
ceballos <>
2008-10-06 16:57:16 +0000

    Adding 3 new modules

--- PhysicsMod/src/MitAnaPhysicsModLinkDef.h
+++ PhysicsMod/src/MitAnaPhysicsModLinkDef.h
@@ -1,9 +1,12 @@
-// $Id:  $
+// $Id: MitAnaPhysicsModLinkDef.h,v 1.1 2008/10/06 15:40:14 sixie Exp $
 
 #ifndef PHYSICSMOD_LINKDEF_H
 #define PHYSICSMOD_LINKDEF_H
 #include "MitAna/PhysicsMod/interface/HwwEvtSelMod.h"
 #include "MitAna/PhysicsMod/interface/HwwEvtPreSelMod.h"
+#include "MitAna/PhysicsMod/interface/ttEvtSelMod.h"
+#include "MitAna/PhysicsMod/interface/FwdJetEvtSelMod.h"
+#include "MitAna/PhysicsMod/interface/ZXEvtSelMod.h"
 #endif
  
 #ifdef __CINT__
@@ -16,4 +19,7 @@
 
 #pragma link C++ class mithep::HwwEvtSelMod+;
 #pragma link C++ class mithep::HwwEvtPreSelMod+;
+#pragma link C++ class mithep::ttEvtSelMod+;
+#pragma link C++ class mithep::FwdJetEvtSelMod+;
+#pragma link C++ class mithep::ZXEvtSelMod+;
 #endif
