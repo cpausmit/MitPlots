c38d854b3107f61519433213d7a5b6aa5b703ed3
loizides <>
2008-12-03 17:37:19 +0000

    Add isRemoved.

--- DataTree/interface/EventHeader.h
+++ DataTree/interface/EventHeader.h
@@ -1,9 +1,11 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: EventHeader.h,v 1.7 2008/09/17 04:21:16 loizides Exp $
+// $Id: EventHeader.h,v 1.8 2008/12/02 09:30:11 loizides Exp $
 //
 // EventHeader
 //
-// Class to hold event specific information.
+// Class to hold event specific information. If IsRemoved() returns kTRUE then the event
+// content has been removed from events tree, and only meta information for book keeping
+// purposes have been kept. This is used in conjunction with skimming.
 //
 // Authors: C.Loizides
 //--------------------------------------------------------------------------------------------------
@@ -19,28 +21,30 @@ namespace mithep
   class EventHeader : public DataBase
   {
     public:
-      EventHeader() : fRunNum(0), fEvtNum(0), fLumiSec(0), fRunEntry(-1) {}
+      EventHeader() : fRunNum(0), fEvtNum(0), fLumiSec(0), fRunEntry(-1), fIsRemoved(0) {}
       EventHeader(UInt_t run, UInt_t evt, UInt_t lumi) : 
-        fRunNum(run), fEvtNum(evt), fLumiSec(lumi), fRunEntry(-1)        {}
-      virtual ~EventHeader() {}
+        fRunNum(run), fEvtNum(evt), fLumiSec(lumi), fRunEntry(-1), fIsRemoved(0)        {}
+      ~EventHeader() {}
 
-      UInt_t              EvtNum()       const { return fEvtNum; }
-      UInt_t              LumiSec()      const { return fLumiSec; }
-      Int_t               RunEntry()     const { return fRunEntry; }
-      UInt_t              RunNum()       const { return fRunNum; }
-      void                SetEvtNum(UInt_t i)  { fEvtNum=i; }
-      void                SetLumiSec(UInt_t i) { fLumiSec=i; }
-      void                SetRunEntry(Int_t i) { fRunEntry=i; }
-      void                SetRunNum(UInt_t i)  { fRunNum=i; }
+      UInt_t              EvtNum()          const { return fEvtNum;    }
+      Bool_t              IsRemoved()       const { return fIsRemoved; }
+      UInt_t              LumiSec()         const { return fLumiSec;   }
+      Int_t               RunEntry()        const { return fRunEntry;  }
+      UInt_t              RunNum()          const { return fRunNum;    }
+      void                SetEvtNum(UInt_t i)     { fEvtNum=i;         }
+      void                SetIsRemoved(Bool_t b)  { fIsRemoved = b;    }
+      void                SetLumiSec(UInt_t i)    { fLumiSec=i;        }
+      void                SetRunEntry(Int_t i)    { fRunEntry=i;       }
+      void                SetRunNum(UInt_t i)     { fRunNum=i;         }
 
     protected:
       UInt_t              fRunNum;          //run number
       UInt_t              fEvtNum;          //event number
       UInt_t              fLumiSec;         //luminosity block number
       Int_t               fRunEntry;        //entry for run block
-      
+      Bool_t              fIsRemoved;       //=true if event content was removed (skimming) 
+
     ClassDef(EventHeader, 2) // Event header class
   };
 }
-
 #endif
