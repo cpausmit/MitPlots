f89c4a6a5d7429173436b2773704366e5a4e3661
loizides <>
2008-12-04 13:48:34 +0000

    Have ReqBranch(const)

--- TreeMod/interface/BaseMod.h
+++ TreeMod/interface/BaseMod.h
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: BaseMod.h,v 1.11 2008/12/01 17:42:01 loizides Exp $
+// $Id: BaseMod.h,v 1.12 2008/12/03 17:38:16 loizides Exp $
 //
 // BaseMod
 //
@@ -36,12 +36,15 @@ namespace mithep
       const TriggerObjectCol     *GetHLTObjects(const char *name) const;
       const TriggerObjectsTable  *GetHLTObjectsTable()            const;
       const TriggerTable         *GetHLTTable()                   const;
-      Int_t                       GetNEventsProcessed() const { return fNEventsProcessed; }
+      Int_t                       GetNEventsProcessed()           const { return fNEventsProcessed; }
+      template <class T> const T *GetObjThisEvt(const char *name) const;
       template <class T> T       *GetObjThisEvt(const char *name);
+      template <class T> const T *GetPublicObj(const char *name)  const;
       template <class T> T       *GetPublicObj(const char *name);
       const RunInfo              *GetRunInfo()          const { return GetSel()->GetRunInfo(); }
       const Selector             *GetSel()              const;
       Bool_t                      HasHLTInfo()          const;
+      template <class T> void     ReqBranch(const char *bname, const T *&address);
       Bool_t                      ValidRunInfo()        const { return GetSel()->ValidRunInfo(); } 
       void                        SaveNEventsProcessed(const char *name="hDEvents");
 
@@ -64,6 +67,21 @@ inline const mithep::TriggerObjectCol *mithep::BaseMod::GetHLTObjects(const char
 
 //--------------------------------------------------------------------------------------------------
 template <class T> 
+inline const T *mithep::BaseMod::GetObjThisEvt(const char *name) const
+{
+  // Get published object for the current event.
+
+  T *ret = dynamic_cast<T*>(FindObjThisEvt(name));
+  if (!ret) {
+    SendError(kWarning, "GetObjThisEvent", 
+              "Could not obtain object with name %s and type %s for current event!",
+              name, T::Class_Name());
+  }
+  return ret;
+}
+
+//--------------------------------------------------------------------------------------------------
+template <class T> 
 inline T *mithep::BaseMod::GetObjThisEvt(const char *name)
 {
   // Get published object for the current event.
@@ -72,6 +90,22 @@ inline T *mithep::BaseMod::GetObjThisEvt(const char *name)
   if (!ret) {
     SendError(kWarning, "GetObjThisEvent", 
               "Could not obtain object with name %s and type %s for current event!",
+              name, T::Class_Name());
+  }
+  return ret;
+}
+
+//--------------------------------------------------------------------------------------------------
+template <class T> 
+inline const T *mithep::BaseMod::GetPublicObj(const char *name) const
+{
+  // Get public object.
+
+
+  T *ret = dynamic_cast<T*>(FindPublicObj(name));
+  if (!ret) {
+    SendError(kWarning, "GetPublicObject", 
+              "Could not obtain public object with name %s and type %s!",
               name, T::Class_Name());
   }
   return ret;
@@ -99,5 +133,15 @@ inline const mithep::Selector *mithep::BaseMod::GetSel() const
   // Get pointer to selector.
 
   return static_cast<const Selector*>(GetSelector()); 
+}
+
+//--------------------------------------------------------------------------------------------------
+template <class T>
+inline void mithep::BaseMod::ReqBranch(const char *bname, const T *&address)
+{
+  // Requests that the branch with the specified name be made available
+  // during processing and that it be read in to the address specified.
+
+  TAModule::ReqBranch(bname, const_cast<T*&>(address));
 }
 #endif
