1d176651777f6a4ed74c5d8e38d4a3914373fa31
loizides <>
2008-12-10 11:25:30 +0000

    Prepared for Mit_008, but not enabled yet.

--- DataCont/src/BaseCollection.cc
+++ DataCont/src/BaseCollection.cc
@@ -1,5 +1,19 @@
-// $Id: BaseCollection.cc,v 1.1 2008/06/05 10:06:29 loizides Exp $
+// $Id: BaseCollection.cc,v 1.1 2008/07/29 10:36:21 loizides Exp $
 
 #include "MitAna/DataCont/interface/BaseCollection.h"
+#include <TClass.h>
 
 ClassImp(mithep::BaseCollection)
+
+//--------------------------------------------------------------------------------------------------
+namespace {
+  class BaseCollectionIgnoreTObject {
+    public:
+      BaseCollectionIgnoreTObject() {
+        TClass *cls = TClass::GetClass("mithep::BaseCollection");
+        if (cls) 
+          cls->IgnoreTObjectStreamer();
+      }
+  };
+  //DataCollectionIgnoreTObject dummy; //TODO enable for MIT_008 production
+}
