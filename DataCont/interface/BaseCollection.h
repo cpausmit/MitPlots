cc2cb9f5e1326c9cedf4f1289b58ec66ef2f6d44
loizides <>
2008-12-10 11:25:00 +0000

    Added generic TObject get function to interface.

--- DataCont/interface/BaseCollection.h
+++ DataCont/interface/BaseCollection.h
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: BaseCollection.h,v 1.1 2008/07/29 10:36:20 loizides Exp $
+// $Id: BaseCollection.h,v 1.2 2008/09/10 03:33:26 loizides Exp $
 //
 // BaseCollection
 //
@@ -22,11 +22,13 @@ namespace mithep
       ~BaseCollection() {}
 
       virtual UInt_t               GetEntries()                   const = 0;
+      virtual TObject             *ObjAt(UInt_t idx)                    = 0;
+      virtual const TObject       *ObjAt(UInt_t idx)              const = 0;
       virtual Bool_t               IsOwner()                      const = 0;
       virtual void                 Reset()                              = 0;
       virtual void                 Trim()                               = 0;
 
-    ClassDef(BaseCollection,1) // Base class of all our collections
+    ClassDef(BaseCollection, 1) // Base class of all our collections
   };
 }
 #endif
