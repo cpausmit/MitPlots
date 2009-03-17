2dd7dd7ea6630bed377cb8999e7e1aa77c86064f
loizides <>
2009-03-17 17:36:53 +0000

    Print sim info

--- DataTree/src/MCParticle.cc
+++ DataTree/src/MCParticle.cc
@@ -1,4 +1,4 @@
-// $Id: MCParticle.cc,v 1.4 2008/11/24 14:11:47 loizides Exp $
+// $Id: MCParticle.cc,v 1.5 2009/02/18 15:38:55 loizides Exp $
 
 #include "MitAna/DataTree/interface/MCParticle.h"
 
@@ -61,13 +61,14 @@ void MCParticle::Print(Option_t *opt) const
 {
   // Print particle kinematics. In case option "l" is given then also print info about daughters.
 
-  printf("id=%5d st=%2d nd=%3d gen=%d px=%.3f py=%.3f pz=%.3f e=%.3f\n", 
-         PdgId(), Status(), NDaughters(), IsGenerated(), Px(), Py(), Pz(), E());
+  printf("id=%5d st=%2d nd=%3d gen=%d sim=%d px=%.3f py=%.3f pz=%.3f e=%.3f\n", 
+         PdgId(), Status(), NDaughters(), IsGenerated(), IsSimulated(), Px(), Py(), Pz(), E());
 
   if (opt && opt[0]=='l') {
     for (UInt_t i=0; i<NDaughters(); ++i) {
-      printf("  %2d -> id=%5d st=%2d gen=%d px=%.3f py=%.3f pz=%.3f e=%.3f\n", 
-             i, Daughter(i)->PdgId(), Daughter(i)->Status(), Daughter(i)->IsGenerated(),
+      printf("  %2d -> id=%5d st=%2d gen=%d sim=%d px=%.3f py=%.3f pz=%.3f e=%.3f\n", 
+             i, Daughter(i)->PdgId(), Daughter(i)->Status(), 
+             Daughter(i)->IsGenerated(), Daughter(i)->IsSimulated(),
              Daughter(i)->Px(), Daughter(i)->Py(), Daughter(i)->Pz(), Daughter(i)->E());
     }
   }
