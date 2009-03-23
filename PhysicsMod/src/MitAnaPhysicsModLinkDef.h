d0f1beda8072addd091e596fb4a2ff735d12ce8c
loizides <>
2009-03-23 14:20:05 +0000

    More incarnations.

--- PhysicsMod/src/MitAnaPhysicsModLinkDef.h
+++ PhysicsMod/src/MitAnaPhysicsModLinkDef.h
@@ -1,4 +1,4 @@
-// $Id: MitAnaPhysicsModLinkDef.h,v 1.9 2009/02/18 15:24:07 bendavid Exp $
+// $Id: MitAnaPhysicsModLinkDef.h,v 1.10 2009/03/11 18:13:11 bendavid Exp $
 
 #ifndef MITANA_PHYSICSMOD_LINKDEF_H
 #define MITANA_PHYSICSMOD_LINKDEF_H
@@ -21,7 +21,9 @@
 #pragma link C++ class mithep::FullExampleMod+;
 #pragma link C++ class mithep::HLTExampleMod+;
 #pragma link C++ class mithep::SimpleExampleMod+;
-#pragma link C++ class mithep::PlotKineMod<mithep::Particle>+;
+
+#pragma link C++ class mithep::PlotKineMod<mithep::CaloJet>+;
+#pragma link C++ class mithep::PlotKineMod<mithep::CaloMet>+;
 #pragma link C++ class mithep::PlotKineMod<mithep::CompositeParticle>+;
 #pragma link C++ class mithep::PlotKineMod<mithep::Conversion>+;
 #pragma link C++ class mithep::PlotKineMod<mithep::Electron>+;
@@ -30,17 +32,21 @@
 #pragma link C++ class mithep::PlotKineMod<mithep::MCParticle>+;
 #pragma link C++ class mithep::PlotKineMod<mithep::Met>+;
 #pragma link C++ class mithep::PlotKineMod<mithep::Muon>+;
+#pragma link C++ class mithep::PlotKineMod<mithep::Particle>+;
 #pragma link C++ class mithep::PlotKineMod<mithep::Photon>+;
 #pragma link C++ class mithep::PlotKineMod<mithep::Track>+;
 #pragma link C++ class mithep::PlotKineMod<mithep::TriggerObject>+;
 
+#pragma link C++ class mithep::PublisherMod<mithep::CaloJet,mithep::Jet>+;
+#pragma link C++ class mithep::PublisherMod<mithep::CaloJet>+;
+#pragma link C++ class mithep::PublisherMod<mithep::CaloMet,mithep::Met>+;
+#pragma link C++ class mithep::PublisherMod<mithep::CaloMet>+;
 #pragma link C++ class mithep::PublisherMod<mithep::CaloTower>+;
 #pragma link C++ class mithep::PublisherMod<mithep::CompositeParticle>+;
 #pragma link C++ class mithep::PublisherMod<mithep::Conversion>+;
 #pragma link C++ class mithep::PublisherMod<mithep::Electron>+;
 #pragma link C++ class mithep::PublisherMod<mithep::GenJet>+;
 #pragma link C++ class mithep::PublisherMod<mithep::Jet>+;
-#pragma link C++ class mithep::PublisherMod<mithep::CaloJet,mithep::Jet>+;
 #pragma link C++ class mithep::PublisherMod<mithep::MCParticle>+;
 #pragma link C++ class mithep::PublisherMod<mithep::Met>+;
 #pragma link C++ class mithep::PublisherMod<mithep::Muon>+;
