f77255204772eb68419a005528e50673abcb6f32
loizides <>
2009-04-07 13:00:58 +0000

    Test case updated

--- macros/examples/runGsfTracksTest.C
+++ macros/examples/runGsfTracksTest.C
@@ -1,4 +1,4 @@
-// $Id: runSimpleExample.C,v 1.10 2009/03/23 14:56:32 loizides Exp $
+// $Id: runGsfTracksTest.C,v 1.1 2009/04/07 11:06:03 loizides Exp $
 
 #if !defined(__CINT__) || defined(__MAKECINT__)
 #include <TROOT.h>
@@ -13,23 +13,24 @@ namespace mithep
   class TestMod : public BaseMod
   {
     public:
-      TestMod() : fSumPt(0), fCount(0) {}
+      TestMod(const char *n="GsfTracks") : fCol(0), fName(n), fSumPt(0), fCount(0) {}
     protected:
       void        Process() { 
-        LoadBranch("GsfTracks"); 
-        for(UInt_t i=0;i<fCol->Entries();++i) {
+        LoadBranch(fName); 
+        for(UInt_t i=0;i<fCol->GetEntries();++i) {
           fSumPt+=fCol->At(i)->Pt();
           ++fCount;
         }
       }
       void        SlaveBegin() { 
-        ReqBranch("GsfTracks",fCol);
+        ReqBranch(fName,fCol);
       }
       void        SlaveTerminate() { 
-        printf("Avg pt found: %f", fSumPt/fCount);
+        printf("TestMod: %s -> Avg pt found: %f (%f/%d)\n", fName.Data(), fSumPt/fCount, fSumPt, fCount);
       }
 
       const TrackCol *fCol;
+      TString         fName;
       Double_t        fSumPt;
       Int_t           fCount;
 
@@ -38,19 +39,24 @@ namespace mithep
 }
 
 //--------------------------------------------------------------------------------------------------
-void runGsfTrackTest(const char *files = "mit-gen_000.root")
+void runGsfTrackTest(const char *files, UInt_t nev=0)
 {
   using namespace mithep;
   gDebugMask  = Debug::kAnalysis;
   gDebugLevel = 1;
 
-  TestMod *tmod = new TestMod;
+  TestMod *tmod1 = new TestMod("GsfTracks");
+  TestMod *tmod2 = new TestMod("Tracks");
   
   // set up analysis
   Analysis *ana = new Analysis;
-  ana->SetSuperModule(tmod);
+  ana->AddSuperModule(tmod1);
+  ana->AddSuperModule(tmod2);
   ana->AddFile(files);
   ana->SetUseHLT(0);
+  if (nev)
+    ana->SetProcessNEvents(nev);
+
   if (gROOT->IsBatch()) 
     ana->SetOutputName("mit-example-hist.root");
   
