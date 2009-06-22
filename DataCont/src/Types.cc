14c0dc365d8210d4657a94314fbab602e02f12ea
loizides <>
2009-06-22 10:14:18 +0000

    Put add namespace. Not sure if this is required but i did this from MitCommon/DataFormats/src/Types.cc as well

--- DataCont/src/Types.cc
+++ DataCont/src/Types.cc
@@ -1,18 +1,20 @@
-// $Id: Ref.cc,v 1.1 2009/02/17 14:57:29 bendavid Exp $
+// $Id: Types.cc,v 1.1 2009/03/08 12:08:31 loizides Exp $
 
 #include "MitAna/DataCont/interface/Types.h"
 
 namespace {
-  mithep::BitMask8   tmp1;
-  mithep::BitMask16  tmp2;
-  mithep::BitMask32  tmp3;
-  mithep::BitMask48  tmp4;
-  mithep::BitMask64  tmp5;
-  mithep::BitMask128 tmp6;
-  mithep::BitMask256 tmp7;
+  namespace {
+    mithep::BitMask8   tmp1;
+    mithep::BitMask16  tmp2;
+    mithep::BitMask32  tmp3;
+    mithep::BitMask48  tmp4;
+    mithep::BitMask64  tmp5;
+    mithep::BitMask128 tmp6;
+    mithep::BitMask256 tmp7;
 
-  mithep::FArrDouble   tmp8;
-  mithep::FArrDouble32 tmp9;
-  mithep::FArrInt      tmp10;
-  mithep::FArrUInt     tmp11;
+    mithep::FArrDouble   tmp8;
+    mithep::FArrDouble32 tmp9;
+    mithep::FArrInt      tmp10;
+    mithep::FArrUInt     tmp11;
+  }
 }
