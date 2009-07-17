05b0cd400e67b504e06c05519d81905d5eb8380c
loizides <>
2009-07-17 21:01:27 +0000

    Removed l1 entry since we have all info in the HLT

--- DataTree/interface/RunInfo.h
+++ DataTree/interface/RunInfo.h
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: RunInfo.h,v 1.9 2008/12/09 17:47:00 loizides Exp $
+// $Id: RunInfo.h,v 1.10 2009/02/18 15:38:54 loizides Exp $
 //
 // RunInfo
 //
@@ -18,23 +18,20 @@ namespace mithep
   class RunInfo : public DataBase
   {
     public:
-      RunInfo() : fRunNum(0), fL1Entry(0), fHltEntry(0) {}
-      RunInfo(UInt_t run) : fRunNum(run), fL1Entry(0), fHltEntry(0) {}
+      RunInfo() : fRunNum(0), fHltEntry(0) {}
+      RunInfo(UInt_t run) : fRunNum(run), fHltEntry(0) {}
 
       Int_t               HltEntry()     const { return fHltEntry; }
-      Int_t               L1Entry()      const { return fL1Entry;  }
       EObjType            ObjType()      const { return kRunInfo;  }      
       UInt_t              RunNum()       const { return fRunNum;   }
-      void                SetL1Entry(Int_t i)  { fL1Entry=i;       }
       void                SetHltEntry(Int_t i) { fHltEntry=i;      }
       void                SetRunNum(UInt_t i)  { fRunNum=i;        }
 
     protected:
       UInt_t              fRunNum;          //run number
-      Int_t               fL1Entry;         //entry for L1 block
       Int_t               fHltEntry;        //entry for HLT block
 
-    ClassDef(RunInfo, 1) // Run info class
+    ClassDef(RunInfo, 2) // Run info class
   };
 }
 #endif
