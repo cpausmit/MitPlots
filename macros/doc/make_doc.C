fd783a4b6d4370c0dca3bb5bd72a970c9c5e1aa8
loizides <>
2009-08-12 08:17:29 +0000

    Use MyThml from MitAna/Utils

--- macros/doc/make_doc.C
+++ macros/doc/make_doc.C
@@ -1,99 +1,14 @@
-// $Id: make_doc.C,v 1.8 2009/07/23 11:13:49 loizides Exp $
+// $Id: make_doc.C,v 1.9 2009/08/11 23:11:30 loizides Exp $
 
-#if !defined(__CINT__) || defined(__MAKECINT__)
-#include <iostream>
-#include <fstream>
-#include <TError.h>
-#include <THashList.h>
-#include <THtml.h>
-#include <TList.h>
-#include <TNamed.h>
-#include <TRegexp.h>
-#include <TROOT.h>
-#include <TSystem.h>
-#include <TDirectory.h>
-#include <TString.h>
-#include <TPRegexp.h>
-#include <TObjArray.h>
-#include <TObjString.h>
-#include <TEnv.h>
-#include "MitAna/Utils/interface/THtml.h"
-#include "MitAna/Utils/interface/TDocInfo.h"
-#endif
-
-class MyHtml : public THtml
-{
-  public:
-    class MyModDef: public TModuleDefinition {
-    public:
-      bool GetModule(TClass* cl, TString& out_modulename) const {
-        TString cn(cl->GetName());
-        if (!cn.BeginsWith("mithep") && !cn.BeginsWith("TAM")) {
-          return 0;
-          //return TModuleDefinition::GetModule(cl, out_modulename);
-        }
-        TString tmp(cl->GetDeclFileName());
-        Ssiz_t fst = tmp.Index("/Mit")+1;
-        Ssiz_t snd = tmp.Index("/",tmp.Index("/",fst)+1);
-        out_modulename = tmp(fst,snd-fst);
-        return 1;
-      }
-
-      ClassDef(MyModDef, 0) // MyModDef class for BAMBU module definition
-    };
-
-    MyHtml() 
-    {
-      MyModDef m;
-      SetModuleDefinition(m);
-    }
-    void SetIncludePath(const char *p) { fPathInfo.fIncludePath=p; }
-    void GetModuleNameForClass(TString& module, TClass* cl) const
-    {
-      module = "NOTUSED";
-        TString cn(cl->GetName());
-      if (!cn.BeginsWith("mithep") && !cn.BeginsWith("TAM")) {
-        return;
-      }
-      TString tmp(cl->GetDeclFileName());
-      Ssiz_t fst = tmp.Index("/Mit")+1;
-      Ssiz_t snd = tmp.Index("/",tmp.Index("/",fst)+1);
-      module = tmp(fst,snd-fst);
-      return;
-    }
-
-    void RunAll(Bool_t force, const char *filter) 
-    {
-      CreateListOfClasses(filter);
-      TModuleDocInfo* module = 
-        dynamic_cast<TModuleDocInfo*>(GetListOfModules()->FindObject("MitCommon"));
-      if (module)
-        module->SetSelected();
-      THtml::MakeAll(force,filter);
-    }
-
-  ClassDef(MyHtml, 0) // MyHtml class for BAMBU docu
-};
-
-void load_libs(const char *ln)
-{
-  TDirectory::TContext context(0); // automatically restore gDirectory
-  TRegexp re(Form("lib%s*.so", ln), true);
-  TString lpath(gSystem->ExpandPathName("$CMSSW_BASE/lib/$SCRAM_ARCH"));
-  void *dir = gSystem->OpenDirectory(lpath);
-  if(!dir) return;
-  while(const char *file = gSystem->GetDirEntry(dir)) {
-    TString s(file);
-    if (s.Index(re) == kNPOS)
-      continue;
-    ::Info("make_doc","Loading %s", file);
-    gSystem->Load(file);
-  }
-  gSystem->FreeDirectory(dir);
-}
-
+// Since we do use our own THtml code from MitAna/Utisl, we 
+// on purpose do not compile this macro anymore.
 void make_doc()
 {
+  gROOT->ProcessLine(".L $CMSSW_BASE/src/MitAna/macros/setRootEnv.C+");
+  loadLibraries("libMitCommon*");
+  loadLibraries("libMitAna*");
+  loadLibraries("libMitPhysics*");
+
   MyHtml h;
   h.SetProductName("MitRoot");
 
--- macros/doc/rootlogon.C
+++
@@ -1,5 +0,0 @@
-// $Id: rootlogon.C,v 1.1 2009/07/16 13:13:55 loizides Exp $
-
-{ 
-  gROOT->Macro("$CMSSW_BASE/src/MitAna/macros/setRootEnv.C+");
-}
