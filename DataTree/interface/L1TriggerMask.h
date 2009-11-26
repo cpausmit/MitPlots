20fd1b369d6354346a11368bda8d056f5eef399b
loizides <>
2009-11-26 21:42:56 +0000

    Store bx, default = 0.

--- DataTree/interface/L1TriggerMask.h
+++ DataTree/interface/L1TriggerMask.h
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: L1TriggerMask.h,v 1.1 2009/03/08 12:10:42 loizides Exp $
+// $Id: L1TriggerMask.h,v 1.1 2009/11/19 14:06:53 loizides Exp $
 //
 // L1TriggerMask
 //
@@ -19,21 +19,24 @@ namespace mithep
   class L1TriggerMask : public DataBase
   {
     public:
-      L1TriggerMask()                                      {}
-      L1TriggerMask(const BitMask64 &mask) : fMask(mask)   {} 
+      L1TriggerMask() : fBx(0) {}
+      L1TriggerMask(const BitMask64 &mask) : fMask(mask), fBx(0) {} 
 
       Bool_t                At(UInt_t n)            const { return fMask.TestBit(n); }
+      Char_t                Bx()                    const { return fBx;              }
       const BitMask64      &Get()                   const { return fMask;            }
       EObjType              ObjType()               const { return kL1TriggerMask;   }      
       void                  Print(Option_t *opt="") const { fMask.Print(opt);        }
       void                  SetBits(const BitMask64 &m)   { fMask.SetBits(m);        }
+      void                  SetBx(Char_t bx)              { fBx = bx;                }
       UInt_t                Size()                  const { return fMask.Size();     }
       Bool_t                TestBit(UInt_t n)       const { return fMask.TestBit(n); }
 
     protected:
       BitMask64             fMask; //trigger mask
+      Char_t                fBx;   //bunch crossing (-2,-1,0,1,2)
 
-    ClassDef(L1TriggerMask, 1) // L1 trigger mask 
+    ClassDef(L1TriggerMask, 2) // L1 trigger mask 
   };
 }
 #endif
