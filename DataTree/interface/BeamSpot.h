897ab5cdbf00053c35599f7ee12e254f7f73545f
bendavid <>
2009-11-30 13:42:40 +0000

    BeamWidth now stored seperately for x and y axes

--- DataTree/interface/BeamSpot.h
+++ DataTree/interface/BeamSpot.h
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: BeamSpot.h,v 1.3 2009/02/18 15:38:54 loizides Exp $
+// $Id: BeamSpot.h,v 1.4 2009/03/18 15:44:31 loizides Exp $
 //
 // BeamSpot
 //
@@ -19,47 +19,53 @@ namespace mithep
   {
     public:
       BeamSpot() : 
-        fSigmaZ(0), fBeamWidth(0), fDxDz(0), fDyDz(0),
-        fSigmaZErr(0), fBeamWidthErr(0), fDxDzErr(0), fDyDzErr(0) {}
+        fSigmaZ(0), fBeamWidthX(0), fBeamWidthY(0), fDxDz(0), fDyDz(0),
+        fSigmaZErr(0), fBeamWidthXErr(0), fBeamWidthYErr(0), fDxDzErr(0), fDyDzErr(0) {}
       BeamSpot(Double_t x, Double_t y, Double_t z) : 
-        BaseVertex(x,y,z), fSigmaZ(0), fBeamWidth(0), fDxDz(0), fDyDz(0),
-        fSigmaZErr(0), fBeamWidthErr(0), fDxDzErr(0), fDyDzErr(0) {}
+        BaseVertex(x,y,z), fSigmaZ(0), fBeamWidthX(0), fBeamWidthY(0), fDxDz(0), fDyDz(0),
+        fSigmaZErr(0), fBeamWidthXErr(0), fBeamWidthYErr(0), fDxDzErr(0), fDyDzErr(0) {}
       BeamSpot(Double_t x, Double_t y, Double_t z, Double_t xErr, Double_t yErr, Double_t zErr) :
-        BaseVertex(x,y,z,xErr,yErr,zErr), fSigmaZ(0), fBeamWidth(0), fDxDz(0), fDyDz(0),
-        fSigmaZErr(0), fBeamWidthErr(0), fDxDzErr(0), fDyDzErr(0) {}
+        BaseVertex(x,y,z,xErr,yErr,zErr), fSigmaZ(0), fBeamWidthX(0), fBeamWidthY(0), fDxDz(0), fDyDz(0),
+        fSigmaZErr(0), fBeamWidthXErr(0), fBeamWidthYErr(0), fDxDzErr(0), fDyDzErr(0) {}
       BeamSpot(const ThreeVector &pos) :
-        BaseVertex(pos), fSigmaZ(0), fBeamWidth(0), fDxDz(0), fDyDz(0),
-        fSigmaZErr(0), fBeamWidthErr(0), fDxDzErr(0), fDyDzErr(0) {}
+        BaseVertex(pos), fSigmaZ(0), fBeamWidthX(0), fBeamWidthY(0), fDxDz(0), fDyDz(0),
+        fSigmaZErr(0), fBeamWidthXErr(0), fBeamWidthYErr(0), fDxDzErr(0), fDyDzErr(0) {}
       
-      Double_t            BeamWidth()                            const { return fBeamWidth;    }
-      Double_t            BeamWidthErr()                         const { return fBeamWidthErr; }
-      Double_t            DxDz()                                 const { return fDxDz;         }
-      Double_t            DyDz()                                 const { return fDyDz;         }
-      Double_t            DxDzErr()                              const { return fDxDzErr;      }
-      Double_t            DyDzErr()                              const { return fDyDzErr;      }
-      EObjType            ObjType()                              const { return kBeamSpot;     }
-      Double_t            SigmaZ()                               const { return fSigmaZ;       } 
-      Double_t            SigmaZErr()                            const { return fSigmaZErr;    } 
-      void                SetBeamWidth(Double_t beamWidth)             { fBeamWidth = beamWidth;   }
-      void                SetBeamWidthErr(Double_t beamWErr)           { fBeamWidthErr = beamWErr; }
-      void                SetDxDz(Double_t dxDz)                       { fDxDz = dxDz;             }
-      void                SetDyDz(Double_t dyDz)                       { fDyDz = dyDz;             }
-      void                SetDxDzErr(Double_t dxDzErr)                 { fDxDzErr = dxDzErr;       }
-      void                SetDyDzErr(Double_t dyDzErr)                 { fDyDzErr = dyDzErr;       }
-      void                SetSigmaZ(Double_t sigmaZ)                   { fSigmaZ = sigmaZ;         }
-      void                SetSigmaZErr(Double_t sigmaZErr)             { fSigmaZErr = sigmaZErr;   }
+      Double_t            BeamWidthX()                           const { return fBeamWidthX;    }
+      Double_t            BeamWidthY()                           const { return fBeamWidthY;    }
+      Double_t            BeamWidthXErr()                        const { return fBeamWidthXErr; }
+      Double_t            BeamWidthYErr()                        const { return fBeamWidthYErr; }
+      Double_t            DxDz()                                 const { return fDxDz;          }
+      Double_t            DyDz()                                 const { return fDyDz;          }
+      Double_t            DxDzErr()                              const { return fDxDzErr;       }
+      Double_t            DyDzErr()                              const { return fDyDzErr;       }
+      EObjType            ObjType()                              const { return kBeamSpot;      }
+      Double_t            SigmaZ()                               const { return fSigmaZ;        } 
+      Double_t            SigmaZErr()                            const { return fSigmaZErr;     } 
+      void                SetBeamWidthX(Double_t beamWidth)            { fBeamWidthX = beamWidth;   }
+      void                SetBeamWidthY(Double_t beamWidth)            { fBeamWidthY = beamWidth;   }
+      void                SetBeamWidthXErr(Double_t beamWErr)          { fBeamWidthXErr = beamWErr; }
+      void                SetBeamWidthYErr(Double_t beamWErr)          { fBeamWidthYErr = beamWErr; }
+      void                SetDxDz(Double_t dxDz)                       { fDxDz = dxDz;              }
+      void                SetDyDz(Double_t dyDz)                       { fDyDz = dyDz;              }
+      void                SetDxDzErr(Double_t dxDzErr)                 { fDxDzErr = dxDzErr;        }
+      void                SetDyDzErr(Double_t dyDzErr)                 { fDyDzErr = dyDzErr;        }
+      void                SetSigmaZ(Double_t sigmaZ)                   { fSigmaZ = sigmaZ;          }
+      void                SetSigmaZErr(Double_t sigmaZErr)             { fSigmaZErr = sigmaZErr;    }
             
     protected:
       Double32_t          fSigmaZ;        //[0,0,14]z width of beamspot
-      Double32_t          fBeamWidth;     //[0,0,14]transverse width of beamspot
+      Double32_t          fBeamWidthX;    //[0,0,14]transverse width of beamspot (x-axis)
+      Double32_t          fBeamWidthY;    //[0,0,14]transverse width of beamspot (y-axis)
       Double32_t          fDxDz;          //[0,0,14]x-z slope of beamline
       Double32_t          fDyDz;          //[0,0,14]y-z slope of beamline
       Double32_t          fSigmaZErr;     //[0,0,14]error of z width
-      Double32_t          fBeamWidthErr;  //[0,0,14]error of transverse width
+      Double32_t          fBeamWidthXErr; //[0,0,14]error of transverse width (x-axis)
+      Double32_t          fBeamWidthYErr; //[0,0,14]error of transverse width (y-axis)
       Double32_t          fDxDzErr;       //[0,0,14]error of x-z slope
       Double32_t          fDyDzErr;       //[0,0,14]error of y-z slope
 	
-    ClassDef(BeamSpot, 1) // Beam spot class
+    ClassDef(BeamSpot, 2) // Beam spot class
   };
 }
 #endif
