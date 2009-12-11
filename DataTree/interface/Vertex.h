46f626794185c2b2efc2b44072c777017010dc58
bendavid <>
2009-12-11 17:44:59 +0000

    Add track RefArray to PV

--- DataTree/interface/Vertex.h
+++ DataTree/interface/Vertex.h
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: Vertex.h,v 1.11 2009/11/26 21:42:30 loizides Exp $
+// $Id: Vertex.h,v 1.12 2009/12/11 13:55:46 loizides Exp $
 //
 // Vertex
 //
@@ -13,37 +13,47 @@
  
 #include <TMath.h>
 #include "MitAna/DataTree/interface/BaseVertex.h"
+#include "MitAna/DataCont/interface/RefArray.h"
+#include "MitAna/DataTree/interface/Track.h"
 
 namespace mithep 
 {
   class Vertex : public BaseVertex
   {
     public:
-      Vertex() : fChi2(0), fNdof(0), fNTracks(0) {}
+      Vertex() : fChi2(0), fIsValid(kFALSE), fNdof(0), fNTracks(0) {}
       Vertex(Double_t x, Double_t y, Double_t z) : 
-        BaseVertex(x,y,z), fChi2(0), fNdof(0), fNTracks(0) {}
+        BaseVertex(x,y,z), fChi2(0), fIsValid(kFALSE), fNdof(0), fNTracks(0) {}
       Vertex(Double_t x, Double_t y, Double_t z, Double_t xErr, Double_t yErr, Double_t zErr) :
-        BaseVertex(x,y,z,xErr,yErr,zErr), fChi2(0), fNdof(0), fNTracks(0) {}
+        BaseVertex(x,y,z,xErr,yErr,zErr), fChi2(0), fIsValid(kFALSE), fNdof(0), fNTracks(0) {}
       Vertex(const ThreeVector &pos) : 
         BaseVertex(pos), fChi2(0), fNdof(0), fNTracks(0) {}
       
-      Double_t            Chi2()       const { return fChi2;                    } 
+      void                AddTrack(const Track *t) { fTracks.Add(t);           }
+      Double_t            Chi2()      const { return fChi2;                    } 
       Int_t               Compare(const TObject *o) const;
+      Bool_t              HasTrack(const Track *t)  const { return fTracks.HasObject(t); }
       Bool_t              IsSortable() const { return kTRUE;                    }
-      UShort_t            Ndof()       const { return fNdof;                    }
-      UInt_t              NTracks()    const { return fNTracks;                 }
-      EObjType            ObjType()    const { return kVertex;                  }      
-      Double_t            Prob()       const { return TMath::Prob(fChi2,fNdof); }
-      void                SetChi2(Double_t chi2)     { fChi2 = chi2;     }
-      void                SetNdof(UShort_t nDof)     { fNdof = nDof;     } 
-      void                SetNTracks(UShort_t ntrks) { fNTracks = ntrks; }
+      Bool_t              IsValid()   const { return fIsValid;                 }
+      UShort_t            Ndof()      const { return fNdof;                    }
+      UInt_t              NTracksFit() const { return fNTracks;                }
+      UInt_t              NTracks()   const { return fTracks.Entries();        }
+      EObjType            ObjType()   const { return kVertex;                  }      
+      Double_t            Prob()      const { return TMath::Prob(fChi2,fNdof); }
+      void                SetChi2(Double_t chi2)     { fChi2 = chi2;           }
+      void                SetIsValid(Bool_t b)       { fIsValid = b;           }
+      void                SetNdof(UShort_t nDof)     { fNdof = nDof;           } 
+      void                SetNTracksFit(UInt_t n)    { fNTracks = n;           }
+      const Track        *Trk(UInt_t i) const        { return fTracks.At(i);   }
             
     protected:
       Double32_t          fChi2;     //[0,0,12]chi squared of conversion vertex fit
+      Bool_t              fIsValid;  //is vertex valid
       UShort_t            fNdof;     //number of degrees of freedom of conversion vertex fit
       UShort_t            fNTracks;  //number of tracks used for the fit
+      RefArray<Track>     fTracks;   //tracks associated with the PV
 	
-    ClassDef(Vertex, 1) // Vertex class
+    ClassDef(Vertex, 2) // Vertex class
   };
 }
 
