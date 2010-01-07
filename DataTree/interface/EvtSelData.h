1a671c538c27484ba992374b51a260c3480598bd
loizides <>
2010-01-07 17:09:03 +0000

    Extended event selection data

--- DataTree/interface/EvtSelData.h
+++ DataTree/interface/EvtSelData.h
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: EvtSelData.h,v 1.4 2009/12/13 08:48:58 loizides Exp $
+// $Id: EvtSelData.h,v 1.5 2010/01/05 16:39:44 edwenger Exp $
 //
 // EvtSelData
 //
@@ -24,14 +24,19 @@ namespace mithep
                       fCaNeg(0), fCaPos(0), fCaNegTime(0), fCaPosTime(0),
                       fZdcNeg(0), fZdcPos(0), fZdcNegTime(0), fZdcPosTime(0),
                       fPxbHits(0), fPxHits(0), fClusVtxQual(0), fClusVtxDiff(0),
-	              fHPTrkFrac(0) {}
+                      fNHfNegHits(0), fNHfPosHits(0), fNHfTowersP(0), fNHfTowersN(0),
+                      fSumEsubEpPos(0), fSumEaddEpPos(0), fSumEsubEpNeg(0), 
+                      fSumEaddEpNeg(0), fSumHfEsubEpPos(0), fSumHfEaddEpPos(0), 
+                      fSumHfEsubEpNeg(0), fSumHfEaddEpNeg(0), fHPTrkFrac(0) {}
       EvtSelData(Double_t HcalNeg, Double_t HcalPos,
                  Double_t HfNeg, Double_t HfPos, Double_t HfNegTime, Double_t HfPosTime,
                  Double_t CaNeg, Double_t CaPos, Double_t CaNegTime, Double_t CaPosTime,
                  Double_t ZdcNeg, Double_t ZdcPos, Double_t ZdcNegTime, Double_t ZdcPosTime,
                  Int_t PxbHits, Int_t PxHits, Double_t ClusVtxQual, Double_t ClusVtxDiff,
-		 Double_t HPTrkFrac);
-                 
+                 Int_t nHfNegHits, Int_t nHfPosHits, Int_t nHfTowersP, Int_t nHfTowersN,
+                 Double_t sumEsubEpPos, Double_t sumEaddEpPos, Double_t sumEsubEpNeg, 
+                 Double_t sumEaddEpNeg, Double_t sumHfEsubEpPos, Double_t sumHfEaddEpPos, 
+                 Double_t sumHfEsubEpNeg, Double_t sumHfEaddEpNeg, Double_t HPTrkFrac);
       ~EvtSelData() {}
 
       Double_t      HcalNeg()       const { return fHcalNeg;    }
@@ -59,28 +64,44 @@ namespace mithep
                          Double_t CaNeg, Double_t CaPos, Double_t CaNegTime, Double_t CaPosTime,
                          Double_t ZdcNeg, Double_t ZdcPos, Double_t ZdcNegTime, Double_t ZdcPosTime,
                          Int_t PxbHits, Int_t PxHits, Double_t ClusVtxQual, Double_t ClusVtxDiff,
+                         Int_t nHfNegHits, Int_t nHfPosHits, Int_t nHfTowersP, Int_t nHfTowersN,
+                         Double_t sumEsubEpPos, Double_t sumEaddEpPos, Double_t sumEsubEpNeg, 
+                         Double_t sumEaddEpNeg, Double_t sumHfEsubEpPos, Double_t sumHfEaddEpPos, 
+                         Double_t sumHfEsubEpNeg, Double_t sumHfEaddEpNeg,
 			 Double_t HPTrkFrac);
 
     protected:
-      Double_t      fHcalNeg;     //energy HCAL negative side
-      Double_t      fHcalPos;     //energy HCAL positive side
-      Double_t      fHfNeg;       //energy HF negative side
-      Double_t      fHfPos;       //energy HF positive side
-      Double_t      fHfNegTime;   //energy weighted HF time on negative side 
-      Double_t      fHfPosTime;   //energy weighted HF time on positive side 
-      Double_t      fCaNeg;       //energy CASTOR negative side
-      Double_t      fCaPos;       //energy CASTOR positive side
-      Double_t      fCaNegTime;   //energy weighted CASTOR time on negative side 
-      Double_t      fCaPosTime;   //energy weighted CASTOR time on positive side 
-      Double_t      fZdcNeg;      //energy ZDC negative side
-      Double_t      fZdcPos;      //energy ZDC positive side
-      Double_t      fZdcNegTime;  //energy weighted ZDC time on negative side 
-      Double_t      fZdcPosTime;  //energy weighted ZDC time on positive side 
-      Int_t         fPxbHits;     //number of pixel rechits in the three barrel layers
-      Int_t         fPxHits;      //number of pixel rechits in all barrel and forward layers
-      Double_t      fClusVtxQual; //incompatibility of pixel cluster shapes with vertex (ratio)
-      Double_t      fClusVtxDiff; //incompatibility of pixel cluster shapes with vertex (diff)
-      Double_t      fHPTrkFrac;   //fraction of high-purity tracks out of all with "loose" cuts
+      Double_t      fHcalNeg;        //energy HCAL negative side
+      Double_t      fHcalPos;        //energy HCAL positive side
+      Double_t      fHfNeg;          //energy HF negative side
+      Double_t      fHfPos;          //energy HF positive side
+      Double_t      fHfNegTime;      //energy weighted HF time on negative side 
+      Double_t      fHfPosTime;      //energy weighted HF time on positive side 
+      Double_t      fCaNeg;          //energy CASTOR negative side
+      Double_t      fCaPos;          //energy CASTOR positive side
+      Double_t      fCaNegTime;      //energy weighted CASTOR time on negative side 
+      Double_t      fCaPosTime;      //energy weighted CASTOR time on positive side 
+      Double_t      fZdcNeg;         //energy ZDC negative side
+      Double_t      fZdcPos;         //energy ZDC positive side
+      Double_t      fZdcNegTime;     //energy weighted ZDC time on negative side 
+      Double_t      fZdcPosTime;     //energy weighted ZDC time on positive side 
+      Int_t         fPxbHits;        //number of pixel rechits in the three barrel layers
+      Int_t         fPxHits;         //number of pixel rechits in all barrel and forward layers
+      Double_t      fClusVtxQual;    //incompatibility of pixel cluster shapes with vertex (ratio)
+      Double_t      fClusVtxDiff;    //incompatibility of pixel cluster shapes with vertex (diff)
+      Int_t         fNHfNegHits;     //hf neg hits above threshold
+      Int_t         fNHfPosHits;     //hf pos hits above threshold
+      Int_t         fNHfTowersP;     //hf neg calo towers above threshold
+      Int_t         fNHfTowersN;     //hf pos calo towers above threshold
+      Double_t      fSumEsubEpPos;   //sum E sub Ep for pos calo towers
+      Double_t      fSumEaddEpPos;   //sum E add Ep for pos calo towers
+      Double_t      fSumEsubEpNeg;   //sum E sub Ep for neg calo towers
+      Double_t      fSumEaddEpNeg;   //sum E add Ep for neg calo towers
+      Double_t      fSumHfEsubEpPos; //sum E sub Ep for pos hf calo towers
+      Double_t      fSumHfEaddEpPos; //sum E add Ep for pos hf calo towers
+      Double_t      fSumHfEsubEpNeg; //sum E sub Ep for neg hf calo towers
+      Double_t      fSumHfEaddEpNeg; //sum E add Ep for neg hf calo towers
+      Double_t      fHPTrkFrac;      //fraction of high-purity tracks out of all with "loose" cuts
 
     ClassDef(EvtSelData, 3) // Event selection data
   };
--- DataTree/src/EvtSelData.cc
+++ DataTree/src/EvtSelData.cc
@@ -1,4 +1,4 @@
-// $Id: EvtSelData.cc,v 1.2 2009/12/09 12:48:34 edwenger Exp $
+// $Id: EvtSelData.cc,v 1.3 2010/01/05 16:39:44 edwenger Exp $
 
 #include "MitAna/DataTree/interface/EvtSelData.h"
 
@@ -12,12 +12,21 @@ EvtSelData::EvtSelData(Double_t HcalNeg, Double_t HcalPos,
                        Double_t CaNeg, Double_t CaPos, Double_t CaNegTime, Double_t CaPosTime,
                        Double_t ZdcNeg, Double_t ZdcPos, Double_t ZdcNegTime, Double_t ZdcPosTime,
                        Int_t PxbHits, Int_t PxHits, Double_t ClusVtxQual, Double_t ClusVtxDiff,
+                       Int_t nHfNegHits, Int_t nHfPosHits, Int_t nHfTowersP, Int_t nHfTowersN,
+                       Double_t sumEsubEpPos, Double_t sumEaddEpPos, Double_t sumEsubEpNeg, 
+                       Double_t sumEaddEpNeg, Double_t sumHfEsubEpPos, Double_t sumHfEaddEpPos, 
+                       Double_t sumHfEsubEpNeg, Double_t sumHfEaddEpNeg,
 		       Double_t HPTrkFrac)
   :   fHcalNeg(HcalNeg), fHcalPos(HcalPos),
       fHfNeg(HfNeg), fHfPos(HfPos), fHfNegTime(HfNegTime), fHfPosTime(HfPosTime),
       fCaNeg(CaNeg), fCaPos(CaPos), fCaNegTime(CaNegTime), fCaPosTime(CaPosTime),
       fZdcNeg(ZdcNeg), fZdcPos(ZdcPos), fZdcNegTime(ZdcNegTime), fZdcPosTime(ZdcPosTime),
       fPxbHits(PxbHits), fPxHits(PxHits), fClusVtxQual(ClusVtxQual), fClusVtxDiff(ClusVtxDiff),
+      fNHfNegHits(nHfNegHits), fNHfPosHits(nHfPosHits), 
+      fNHfTowersP(nHfTowersP), fNHfTowersN(nHfTowersN),
+      fSumEsubEpPos(sumEsubEpPos), fSumEaddEpPos(sumEaddEpPos), fSumEsubEpNeg(sumEsubEpNeg), 
+      fSumEaddEpNeg(sumEaddEpNeg), fSumHfEsubEpPos(sumHfEsubEpPos), fSumHfEaddEpPos(sumHfEaddEpPos),
+      fSumHfEsubEpNeg(sumHfEsubEpNeg), fSumHfEaddEpNeg(sumHfEaddEpNeg),
       fHPTrkFrac(HPTrkFrac)
 {
   // Default constructor.
@@ -29,28 +38,43 @@ void EvtSelData::Set (Double_t HcalNeg, Double_t HcalPos,
                       Double_t CaNeg, Double_t CaPos, Double_t CaNegTime, Double_t CaPosTime,
                       Double_t ZdcNeg, Double_t ZdcPos, Double_t ZdcNegTime, Double_t ZdcPosTime,
 		      Int_t PxbHits, Int_t PxHits, Double_t ClusVtxQual, Double_t ClusVtxDiff,
+                      Int_t nHfNegHits, Int_t nHfPosHits, Int_t nHfTowersP, Int_t nHfTowersN,
+                      Double_t sumEsubEpPos, Double_t sumEaddEpPos, Double_t sumEsubEpNeg, 
+                      Double_t sumEaddEpNeg, Double_t sumHfEsubEpPos, Double_t sumHfEaddEpPos, 
+                      Double_t sumHfEsubEpNeg, Double_t sumHfEaddEpNeg,
 		      Double_t HPTrkFrac)
-
 {
   // Set values.
 
-  fHcalNeg     = HcalNeg; 
-  fHcalPos     = HcalPos;
-  fHfNeg       = HfNeg; 
-  fHfPos       = HfPos; 
-  fHfNegTime   = HfNegTime; 
-  fHfPosTime   = HfPosTime;
-  fCaNeg       = CaNeg; 
-  fCaPos       = CaPos; 
-  fCaNegTime   = CaNegTime; 
-  fCaPosTime   = CaPosTime;
-  fZdcNeg      = ZdcNeg; 
-  fZdcPos      = ZdcPos;
-  fZdcNegTime  = ZdcNegTime; 
-  fZdcPosTime  = ZdcPosTime;
-  fPxbHits     = PxbHits;
-  fPxHits      = PxHits;
-  fClusVtxQual = ClusVtxQual;
-  fClusVtxDiff = ClusVtxDiff;
-  fHPTrkFrac   = HPTrkFrac;
+  fHcalNeg        = HcalNeg; 
+  fHcalPos        = HcalPos;
+  fHfNeg          = HfNeg; 
+  fHfPos          = HfPos; 
+  fHfNegTime      = HfNegTime; 
+  fHfPosTime      = HfPosTime;
+  fCaNeg          = CaNeg; 
+  fCaPos          = CaPos; 
+  fCaNegTime      = CaNegTime; 
+  fCaPosTime      = CaPosTime;
+  fZdcNeg         = ZdcNeg; 
+  fZdcPos         = ZdcPos;
+  fZdcNegTime     = ZdcNegTime; 
+  fZdcPosTime     = ZdcPosTime;
+  fPxbHits        = PxbHits;
+  fPxHits         = PxHits;
+  fClusVtxQual    = ClusVtxQual;
+  fClusVtxDiff    = ClusVtxDiff;
+  fNHfNegHits     = nHfNegHits; 
+  fNHfPosHits     = nHfPosHits; 
+  fNHfTowersP     = nHfTowersP; 
+  fNHfTowersN     = nHfTowersN;
+  fSumEsubEpPos   = sumEsubEpPos; 
+  fSumEaddEpPos   = sumEaddEpPos; 
+  fSumEsubEpNeg   = sumEsubEpNeg; 
+  fSumEaddEpNeg   = sumEaddEpNeg; 
+  fSumHfEsubEpPos = sumHfEsubEpPos; 
+  fSumHfEaddEpPos = sumHfEaddEpPos;
+  fSumHfEsubEpNeg = sumHfEsubEpNeg; 
+  fSumHfEaddEpNeg = sumHfEaddEpNeg;
+  fHPTrkFrac      = HPTrkFrac;
 }
