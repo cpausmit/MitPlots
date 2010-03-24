0eaf0fa659a9a3d22434d5d389b9c8051e930071
sixie <>
2010-03-24 15:35:31 +0000

    Add H/E variable

--- DataTree/interface/SuperCluster.h
+++ DataTree/interface/SuperCluster.h
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: SuperCluster.h,v 1.14 2009/11/08 13:04:11 sixie Exp $
+// $Id: SuperCluster.h,v 1.15 2009/11/24 15:57:45 loizides Exp $
 //
 // SuperCluster
 //
@@ -26,41 +26,51 @@ namespace mithep
       SuperCluster() : fEnergy(0), fEtaWidth(0), fPreshowerEnergy(0), 
                        fPhiWidth(0), fRawEnergy(0) {}     
  
-      void	              AddCluster(const BasicCluster *c)          { fClusters.Add(c);       }
-      const BasicCluster     *Cluster(UInt_t i)       const { return fClusters.At(i);              }
-      UInt_t                  ClusterSize()           const { return fClusters.Entries();          }
-      Int_t                   Compare(const TObject *o) const;   
-      Double_t                Energy()                const { return fEnergy;                      }
-      Double_t                Et()                    const;
-      Double_t                Eta()                   const { return fPoint.Eta();                 }
-      Double_t                EtaWidth()              const { return fEtaWidth;                    }
-      Bool_t                  IsSortable()            const { return kTRUE;                        }
-      EObjType                ObjType()               const { return kSuperCluster;                }
-      Double_t                Phi()                   const { return fPoint.Phi();                 }
-      Double_t                PhiWidth()              const { return fPhiWidth;                    }
-      ThreeVectorC            Point()                 const { return fPoint.V();                   }
-      void                    Print(Option_t *opt="") const;
-      Double_t                PreshowerEnergy()       const { return fPreshowerEnergy;             }
-      Double_t                RawEnergy()             const { return fRawEnergy;                   }
-      Double_t                Rho()                   const { return fPoint.Rho();                 }
-      const BasicCluster     *Seed()                  const { return fSeedRef.Obj();               }
-      void	              SetEnergy(Double_t energy)                 { fEnergy = energy;       }
-      void	              SetEtaWidth(Double_t etaWidth)             { fEtaWidth = etaWidth;   }
-      void	              SetPhiWidth(Double_t phiWidth)             { fPhiWidth = phiWidth;   }
-      void	              SetPreshowerEnergy(Double_t e)             { fPreshowerEnergy = e;   }
-      void	              SetRawEnergy(Double_t rawEnergy)           { fRawEnergy = rawEnergy; }
-      void	              SetSeed(const BasicCluster *s)             { fSeedRef = s;           }
-      void	              SetXYZ(Double_t x, Double_t y, Double_t z) { fPoint.SetXYZ(x,y,z);   }
+      void	             AddCluster(const BasicCluster *c)          { fClusters.Add(c);        }
+      const BasicCluster    *Cluster(UInt_t i)       const { return fClusters.At(i);               }
+      UInt_t                 ClusterSize()           const { return fClusters.Entries();           }
+      Int_t                  Compare(const TObject *o) const;   
+      Double_t               Energy()                const { return fEnergy;                       }
+      Double_t               Et()                    const;
+      Double_t               Eta()                   const { return fPoint.Eta();                  }
+      Double_t               EtaWidth()              const { return fEtaWidth;                     }
+      Double_t               HcalDepth1Energy()      const { return fHcalDepth1Energy;             }
+      Double_t               HcalDepth2Energy()      const { return fHcalDepth2Energy;             }
+      Double_t               HadDepth1OverEm()       const { return fHcalDepth1Energy/fEnergy;     }
+      Double_t               HadDepth2OverEm()       const { return fHcalDepth2Energy/fEnergy;     }
+      Double_t               HadOverEm()             const { return (fHcalDepth1Energy+
+                                                                     fHcalDepth2Energy)/fEnergy;   }
+      Bool_t                 IsSortable()            const { return kTRUE;                         }
+      EObjType               ObjType()               const { return kSuperCluster;                 }
+      Double_t               Phi()                   const { return fPoint.Phi();                  }
+      Double_t               PhiWidth()              const { return fPhiWidth;                     }
+      ThreeVectorC           Point()                 const { return fPoint.V();                    }
+      void                   Print(Option_t *opt="") const;
+      Double_t               PreshowerEnergy()       const { return fPreshowerEnergy;              }
+      Double_t               RawEnergy()             const { return fRawEnergy;                    }
+      Double_t               Rho()                   const { return fPoint.Rho();                  }
+      const BasicCluster    *Seed()                  const { return fSeedRef.Obj();                }
+      void	             SetEnergy(Double_t energy)                 { fEnergy = energy;        }
+      void	             SetEtaWidth(Double_t etaWidth)             { fEtaWidth = etaWidth;    }
+      void	             SetPhiWidth(Double_t phiWidth)             { fPhiWidth = phiWidth;    }
+      void	             SetPreshowerEnergy(Double_t e)             { fPreshowerEnergy = e;    }
+      void	             SetRawEnergy(Double_t rawEnergy)           { fRawEnergy = rawEnergy;  }
+      void	             SetHcalDepth1Energy(Double_t x)            { fHcalDepth1Energy = x;   }
+      void	             SetHcalDepth2Energy(Double_t x)            { fHcalDepth2Energy = x;   }
+      void	             SetSeed(const BasicCluster *s)             { fSeedRef = s;            }
+      void	             SetXYZ(Double_t x, Double_t y, Double_t z) { fPoint.SetXYZ(x,y,z);    }
       
     protected:
-      Vect3C                  fPoint;           //centroid Position
-      Double32_t              fEnergy;          //[0,0,14]super cluster energy     
-      Double32_t              fEtaWidth;        //[0,0,14]width in Phi
-      Double32_t              fPreshowerEnergy; //[0,0,14]energy in the preshower
-      Double32_t              fPhiWidth;        //[0,0,14]width in Phi
-      Double32_t              fRawEnergy;       //[0,0,14]super cluster raw energy
-      RefArray<BasicCluster>  fClusters;        //assigned basic clusters
-      Ref<BasicCluster>       fSeedRef;         //seed cluster
+      Vect3C                  fPoint;               //centroid Position
+      Double32_t              fEnergy;              //[0,0,14]super cluster energy     
+      Double32_t              fEtaWidth;            //[0,0,14]width in Phi
+      Double32_t              fPreshowerEnergy;     //[0,0,14]energy in the preshower
+      Double32_t              fPhiWidth;            //[0,0,14]width in Phi
+      Double32_t              fRawEnergy;           //[0,0,14]super cluster raw energy
+      Double32_t              fHcalDepth1Energy;    //[0,0,14] hcal depth1 over ECAL energy
+      Double32_t              fHcalDepth2Energy;    //[0,0,14] hcal depth2 over ECAL energy
+      RefArray<BasicCluster>  fClusters;            //assigned basic clusters
+      Ref<BasicCluster>       fSeedRef;             //seed cluster
 
     ClassDef(SuperCluster, 1) // Super cluster class
   };
