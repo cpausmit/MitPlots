0ab30412e9e9b931254ae7c9a99b130500eb8541
bendavid <>
2010-10-20 20:33:39 +0000

    store properly non-integer vertex ndof

--- DataTree/interface/Vertex.h
+++ DataTree/interface/Vertex.h
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: Vertex.h,v 1.12 2009/12/11 13:55:46 loizides Exp $
+// $Id: Vertex.h,v 1.13 2009/12/11 17:44:59 bendavid Exp $
 //
 // Vertex
 //
@@ -23,11 +23,11 @@ namespace mithep
     public:
       Vertex() : fChi2(0), fIsValid(kFALSE), fNdof(0), fNTracks(0) {}
       Vertex(Double_t x, Double_t y, Double_t z) : 
-        BaseVertex(x,y,z), fChi2(0), fIsValid(kFALSE), fNdof(0), fNTracks(0) {}
+        BaseVertex(x,y,z), fChi2(0), fIsValid(kFALSE), fNdof(0), fAdaptiveNdof(0), fNTracks(0) {}
       Vertex(Double_t x, Double_t y, Double_t z, Double_t xErr, Double_t yErr, Double_t zErr) :
-        BaseVertex(x,y,z,xErr,yErr,zErr), fChi2(0), fIsValid(kFALSE), fNdof(0), fNTracks(0) {}
+        BaseVertex(x,y,z,xErr,yErr,zErr), fChi2(0), fIsValid(kFALSE), fNdof(0), fAdaptiveNdof(0), fNTracks(0) {}
       Vertex(const ThreeVector &pos) : 
-        BaseVertex(pos), fChi2(0), fNdof(0), fNTracks(0) {}
+        BaseVertex(pos), fChi2(0), fNdof(0), fAdaptiveNdof(0), fNTracks(0) {}
       
       void                AddTrack(const Track *t) { fTracks.Add(t);           }
       Double_t            Chi2()      const { return fChi2;                    } 
@@ -35,14 +35,14 @@ namespace mithep
       Bool_t              HasTrack(const Track *t)  const { return fTracks.HasObject(t); }
       Bool_t              IsSortable() const { return kTRUE;                    }
       Bool_t              IsValid()   const { return fIsValid;                 }
-      UShort_t            Ndof()      const { return fNdof;                    }
+      Double_t            Ndof()      const { return (fAdaptiveNdof>0.0 ? fAdaptiveNdof:fNdof); }
       UInt_t              NTracksFit() const { return fNTracks;                }
       UInt_t              NTracks()   const { return fTracks.Entries();        }
       EObjType            ObjType()   const { return kVertex;                  }      
       Double_t            Prob()      const { return TMath::Prob(fChi2,fNdof); }
       void                SetChi2(Double_t chi2)     { fChi2 = chi2;           }
       void                SetIsValid(Bool_t b)       { fIsValid = b;           }
-      void                SetNdof(UShort_t nDof)     { fNdof = nDof;           } 
+      void                SetNdof(Double_t nDof)     { fAdaptiveNdof = nDof;   } 
       void                SetNTracksFit(UInt_t n)    { fNTracks = n;           }
       const Track        *Trk(UInt_t i) const        { return fTracks.At(i);   }
             
@@ -50,10 +50,11 @@ namespace mithep
       Double32_t          fChi2;     //[0,0,12]chi squared of conversion vertex fit
       Bool_t              fIsValid;  //is vertex valid
       UShort_t            fNdof;     //number of degrees of freedom of conversion vertex fit
+      Double32_t          fAdaptiveNdof; //number of degrees of freedom of vertex fit (can be non-integer for weighted components)
       UShort_t            fNTracks;  //number of tracks used for the fit
       RefArray<Track>     fTracks;   //tracks associated with the PV
 	
-    ClassDef(Vertex, 2) // Vertex class
+    ClassDef(Vertex, 3) // Vertex class
   };
 }
 
