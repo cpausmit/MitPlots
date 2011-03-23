cdbdeb23e48519eca8f81d52ae66acf831d42428
mzanetti <>
2011-03-23 19:03:46 +0000

    two values for rho stored

--- DataTree/interface/PileupEnergyDensity.h
+++ DataTree/interface/PileupEnergyDensity.h
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: PileupEnergyDensity.h,v 1.5 2009/11/30 13:42:40 bendavid Exp $
+// $Id: PileupEnergyDensity.h,v 1.1 2011/03/01 14:23:05 mzanetti Exp $
 //
 // PileupEnergyDensity
 //
@@ -18,17 +18,21 @@ namespace mithep
   class PileupEnergyDensity : public DataBase
   {
   public:
-    PileupEnergyDensity() : fRho(0) {}
-    PileupEnergyDensity(Double_t r) : fRho(r) {}
+    PileupEnergyDensity() : fRho(0), fRhoHighEta(0) {}
+    PileupEnergyDensity(Double_t r) : fRho(r), fRhoHighEta(0)  {}
+    PileupEnergyDensity(Double_t r, Double_t rHighEta) : fRho(r), fRhoHighEta(rHighEta)  {}
     
     Double_t  Rho()  const { return fRho; }
+    Double_t  RhoHighEta()  const { return fRhoHighEta; }
     
     virtual PileupEnergyDensity  *MakeCopy()                 const { return new PileupEnergyDensity(*this);   }
     
     void      SetRho(Double_t r)   { fRho = r; }
+    void      SetRhoHighEta(Double_t r)   { fRhoHighEta = r; }
     
   protected:
     Double32_t          fRho;      //rho from fastjet algo
+    Double32_t          fRhoHighEta;      //rho from fastjet algo
     
     ClassDef(PileupEnergyDensity, 1) // Pileup Energy Density class
       };
