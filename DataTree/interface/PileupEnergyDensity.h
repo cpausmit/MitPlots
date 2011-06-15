fdb6fc310eb87afdf0ac21baf0c6c17ed94bf142
bendavid <>
2011-06-15 20:03:18 +0000

    different and more variations of rho

--- DataTree/interface/PileupEnergyDensity.h
+++ DataTree/interface/PileupEnergyDensity.h
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: PileupEnergyDensity.h,v 1.1 2011/03/01 14:23:05 mzanetti Exp $
+// $Id: PileupEnergyDensity.h,v 1.2 2011/03/23 19:03:46 mzanetti Exp $
 //
 // PileupEnergyDensity
 //
@@ -18,23 +18,30 @@ namespace mithep
   class PileupEnergyDensity : public DataBase
   {
   public:
-    PileupEnergyDensity() : fRho(0), fRhoHighEta(0) {}
-    PileupEnergyDensity(Double_t r) : fRho(r), fRhoHighEta(0)  {}
-    PileupEnergyDensity(Double_t r, Double_t rHighEta) : fRho(r), fRhoHighEta(rHighEta)  {}
+    PileupEnergyDensity() : fRho(0), fRhoHighEta(0), fRhoRandom(0), fRhoRandomLowEta(0) {}
     
     Double_t  Rho()  const { return fRho; }
-    Double_t  RhoHighEta()  const { return fRhoHighEta; }
+    Double_t  RhoHighEta()  const { return fRho; }
+    Double_t  RhoLowEta()  const { return fRhoHighEta; }
+    Double_t  RhoRandom() const { return fRhoRandom; }
+    Double_t  RhoRandomLowEta() const { return fRhoRandomLowEta; }
     
     virtual PileupEnergyDensity  *MakeCopy()                 const { return new PileupEnergyDensity(*this);   }
     
     void      SetRho(Double_t r)   { fRho = r; }
-    void      SetRhoHighEta(Double_t r)   { fRhoHighEta = r; }
+    void      SetRhoLowEta(Double_t r)   { fRhoHighEta = r; }
+    void      SetRhoRandom(Double_t r)   { fRhoRandom = r; }
+    void      SetRhoRandomLowEta(Double_t r)   { fRhoRandomLowEta = r; }
+
     
   protected:
     Double32_t          fRho;      //rho from fastjet algo
     Double32_t          fRhoHighEta;      //rho from fastjet algo
+    Double32_t          fRhoRandom;      //rho from fastjet algo
+    Double32_t          fRhoRandomLowEta; //rho from fastjet algo
+
     
-    ClassDef(PileupEnergyDensity, 1) // Pileup Energy Density class
+    ClassDef(PileupEnergyDensity, 2) // Pileup Energy Density class
       };
 }
 #endif
