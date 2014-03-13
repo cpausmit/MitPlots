9c8671bdd60e31a31500679d1fcd19b335559c69
Christoph <paus@mit.edu>
2014-03-13 00:27:15 -0400

    Helper for moving the output coherently.

---
+++ bin/moveOutput.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#===================================================================================================
+# Script to move a given set of output to a new tag name.
+#
+#                                                                             Ch.Paus (Mar 13, 2014)
+#===================================================================================================
+TAG="$1"
+NEW_TAG="$2"
+
+# first we do the histogram output
+
+cd $MIT_PROD_HIST
+mv $TAG $NEW_TAG
+
+cd $NEW_TAG/merged
+pwd
+repfil $TAG $NEW_TAG spacing
+cd -
+
+cd $NEW_TAG/$MIT_PROD_BOOK
+for dir in `echo  ???/*`
+do
+  cd $dir
+  pwd
+  repfil $TAG $NEW_TAG spacing
+  cd -
+done
+
+# next we do the log output
+
+cd $MIT_PROD_LOGS
+mv $TAG $NEW_TAG
+cd -
+
+exit 0
