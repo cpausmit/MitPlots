9334634a58ce2492c5a8ed034faa96393c5786c7
Yutaro Iiyama <yiiyama@mit.edu>
2015-04-02 15:46:11 -0400

    Bugs in ROOT 5.22 are all fixed now. Constantin also did not seem to like gHtml global object but I think it's harmless. Should use ROOT classes instead of copies of them.

--- Utils/src/MitAnaUtilsLinkDef.h
+++ Utils/src/MitAnaUtilsLinkDef.h
@@ -4,12 +4,6 @@
 #define MITANA_UTILS_LINKDEF_H
 #include "MitAna/Utils/interface/MyHtml.h"
 #include "MitAna/Utils/interface/SimpleTable.h"
-#include "MitAna/Utils/interface/TClassDocOutput.h"
-#include "MitAna/Utils/interface/TDocDirective.h"
-#include "MitAna/Utils/interface/TDocInfo.h"
-#include "MitAna/Utils/interface/TDocOutput.h"
-#include "MitAna/Utils/interface/TDocParser.h"
-#include "MitAna/Utils/interface/THtml.h"
 #endif
 
 #ifdef __CINT__
@@ -24,23 +18,4 @@
 #pragma link C++ class MyHtml::MyModDef+;
 #pragma link C++ class mithep::SimpleTable+;
 #pragma link C++ class mithep::SimpleTable::MyParameter+;
-#pragma link C++ class TClassDocInfo;
-#pragma link C++ class TClassDocOutput;
-#pragma link C++ class TDocDirective;
-#pragma link C++ class TDocHtmlDirective;
-#pragma link C++ class TDocLatexDirective;
-#pragma link C++ class TDocMacroDirective;
-#pragma link C++ class TDocOutput;
-#pragma link C++ class TDocParser;
-#pragma link C++ class THtml;
-#pragma link C++ class THtml::TFileDefinition;
-#pragma link C++ class THtml::TFileSysDB;
-#pragma link C++ class THtml::TFileSysDir;
-#pragma link C++ class THtml::TFileSysEntry;
-#pragma link C++ class THtml::TFileSysRoot;
-#pragma link C++ class THtml::THelperBase;
-#pragma link C++ class THtml::TModuleDefinition;
-#pragma link C++ class THtml::TPathDefinition;
-#pragma link C++ class TLibraryDocInfo;
-#pragma link C++ class TModuleDocInfo;
 #endif
