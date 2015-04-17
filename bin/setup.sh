db76b9583ce9086513dad7a706a5046f028fb0e7
Yutaro Iiyama <yiiyama@mit.edu>
2015-04-17 15:39:29 -0400

    adding banner

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,5 +5,9 @@
 #                                                                   Apr 17, 2015 - V1 Y. Iiyama
 #----------------------------------------------------------------------------------------------
 
+echo "*********************"
+echo " MitAna/bin/setup.sh"
+echo "*********************"
+
 # Generate ROOT dictionaries for classes defined in this module
 $CMSSW_BASE/src/MitCommon/bin/genDict.sh MitAna/{Catalog,DataCont,DataTree,DataUtil,PhysicsMod,TAM,TreeMod,Utils,Validation}
