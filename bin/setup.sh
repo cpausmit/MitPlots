6d209a0f4e95c4845248ef56d6baee0a74667f72
Yutaro Iiyama <yiiyama@mit.edu>
2015-04-17 15:40:31 -0400

    adding banner

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,6 +5,10 @@
 #                                                                   Apr 17, 2015 - V1 Y. Iiyama
 #----------------------------------------------------------------------------------------------
 
+echo "***********************"
+echo " MitPlots/bin/setup.sh"
+echo "***********************"
+
 # Generate ROOT dictionaries for classes defined in this module
 $CMSSW_BASE/src/MitCommon/bin/genDict.sh MitPlots/{Style,Input,Plot}
 
