9279a2de3b2e6c317670e34966a2808bd98619cd
Yutaro Iiyama <yiiyama@mit.edu>
2015-04-17 17:01:47 -0400

    Setting ignore TObject Streamer at the first instantiation

--- DataTree/src/DataBase.cc
+++ DataTree/src/DataBase.cc
@@ -1,19 +1,5 @@
 // $Id: DataBase.cc,v 1.1 2008/12/02 09:30:11 loizides Exp $
 
 #include "MitAna/DataTree/interface/DataBase.h"
-#include <TClass.h>
 
 ClassImp(mithep::DataBase)
-
-//--------------------------------------------------------------------------------------------------
-namespace {
-  class DataBaseIgnoreTObject {
-    public:
-      DataBaseIgnoreTObject() {
-        TClass *cls = TClass::GetClass("mithep::DataBase");
-        if (cls) 
-          cls->IgnoreTObjectStreamer();
-      }
-  };
-  DataBaseIgnoreTObject dummy;
-}
