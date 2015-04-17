c0d144bcd16a1b94982c92fa1c96c5d171ac2e75
Yutaro Iiyama <yiiyama@mit.edu>
2015-04-17 15:16:26 -0400

    adding setup script

---
+++ bin/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#----------------------------------------------------------------------------------------------
+# Setup the MitAna package adjusting things that are needed for it to compile and run properly.
+#
+#                                                                   Apr 17, 2015 - V1 Y. Iiyama
+#----------------------------------------------------------------------------------------------
+
+# Generate ROOT dictionaries for classes defined in this module
+$CMSSW_BASE/src/MitCommon/bin/genDict.sh MitAna/{Catalog,DataCont,DataTree,DataUtil,PhysicsMod,TAM,TreeMod,Utils,Validation}
