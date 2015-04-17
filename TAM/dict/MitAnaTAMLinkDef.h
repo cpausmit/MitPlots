6b474d65457ed1878c23b0212d4d1cbab90411f5
Yutaro Iiyama <yiiyama@mit.edu>
2015-04-17 15:21:18 -0400

    moved LinkDef to dict

--- TAM/dict/MitAnaTAMLinkDef.h
+++ TAM/dict/MitAnaTAMLinkDef.h
@@ -1,7 +1,23 @@
 //
-// $Id: LinkDef.h 5395 2009-01-20 12:18:42Z loizides $
+// $Id: MitAnaTAMLinkDef.h,v 1.1 2008/05/27 19:13:21 loizides Exp $
 //
 //
+
+#ifndef __MitAnaTreeModLinkDef__
+#define __MitAnaTreeModLinkDef__
+
+#include "MitAna/TAM/interface/TAMBranchInfo.h"
+#include "MitAna/TAM/interface/TAModule.h"
+#include "MitAna/TAM/interface/TAMObjBranchLoader.h"
+#include "MitAna/TAM/interface/TAMObjLoader.h"
+#include "MitAna/TAM/interface/TAMOutput.h"
+#include "MitAna/TAM/interface/TAMSelector.h"
+#include "MitAna/TAM/interface/TAMTreeBranchLoader.h"
+#include "MitAna/TAM/interface/TAMTreeLoader.h"
+#include "MitAna/TAM/interface/TAMVirtualBranchLoader.h"
+#include "MitAna/TAM/interface/TAMVirtualLoader.h"
+
+#endif
 
 #ifdef __CLING__
 
