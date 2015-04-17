93f1433a5aa910a0bb178a2ac8ff8c194bd5739a
Yutaro Iiyama <yiiyama@mit.edu>
2015-04-17 15:24:08 -0400

    new setup script

---
+++ bin/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#----------------------------------------------------------------------------------------------
+# Setup the MitPlots package adjusting things that are needed for it to compile and run properly.
+#
+#                                                                   Apr 17, 2015 - V1 Y. Iiyama
+#----------------------------------------------------------------------------------------------
+
+# Generate ROOT dictionaries for classes defined in this module
+$CMSSW_BASE/src/MitCommon/bin/genDict.sh MitPlots/{Style,Input,Plot}
+
