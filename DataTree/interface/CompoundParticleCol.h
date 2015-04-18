82b0debec4591508bc3090602d3c003d303a218b
Yutaro Iiyama <yiiyama@mit.edu>
2015-04-18 16:45:33 -0400

    bugfix typo COMPOSITE -> COMPOUND

--- DataTree/interface/CompoundParticleCol.h
+++ DataTree/interface/CompoundParticleCol.h
@@ -1,7 +1,7 @@
 // $Id:$
 
-#ifndef MITANA_DATATREE_COMPOSITEPARTICLECOL_H
-#define MITANA_DATATREE_COMPOSITEPARTICLECOL_H
+#ifndef MITANA_DATATREE_COMPOUNDPARTICLECOL_H
+#define MITANA_DATATREE_COMPOUNDPARTICLECOL_H
 
 #include "MitAna/DataCont/interface/Collection.h"
 #include "MitAna/DataCont/interface/Array.h"
