5e2dd37762bc055687c8a7499bd791a92c2cf050
Yutaro Iiyama <yiiyama@mit.edu>
2015-04-25 17:00:30 -0400

    improve interface for multiple member replacement

--- Utils/interface/ConvertStreamer.h
+++ Utils/interface/ConvertStreamer.h
@@ -1,12 +1,21 @@
 #ifndef MITANA_UTILS_CONVERTSTREAMER_H
 #define MITANA_UTILS_CONVERTSTREAMER_H
 
+#include "RtypesCore.h"
+#include "TString.h"
+
+#include <vector>
+
+class TBuffer;
 class TClass;
 class TStreamerInfo;
 
 namespace mithep {
 
-  bool ConvertStreamer(TClass const*, TStreamerInfo*, char const*);
+  Version_t CheckStreamerVersion(TBuffer&, TClass const*, Version_t);
+  void ReplaceStreamerElement(TClass const*, TStreamerInfo*, char const*);
+  void ConvertStreamer(TClass*, Version_t, void*, std::vector<TString> const&);
+  void ConvertStreamer(TClass*, Version_t, void*, char const*);
 
 }
 
--- Utils/src/ConvertStreamer.cc
+++ Utils/src/ConvertStreamer.cc
@@ -1,8 +1,10 @@
 #include "MitAna/Utils/interface/ConvertStreamer.h"
 
+#include "TBuffer.h"
 #include "TClass.h"
 #include "TStreamerInfo.h"
 #include "TStreamerElement.h"
+#include "TInterpreter.h"
 
 #include <cstring>
 #include <iostream>
@@ -10,14 +12,30 @@
 
 namespace mithep {
 
-  bool
-  ConvertStreamer(TClass const* _class, TStreamerInfo* _onfileInfo, char const* _elemName)
+  Version_t
+  CheckStreamerVersion(TBuffer& _buf, TClass const* _thisClass, Version_t _rt6Version)
+  {
+    unsigned start;
+    Version_t version(_buf.ReadVersionNoCheckSum(&start));
+    _buf.SetBufferOffset(start);
+
+    if(version >= _rt6Version) return 0;
+
+    TStreamerInfo* info(static_cast<TStreamerInfo*>(_thisClass->GetStreamerInfos()->UncheckedAt(version)));
+
+    if(info->IsCompiled()) return 0;
+
+    return version;
+  }
+
+  void
+  ReplaceStreamerElement(TClass const* _class, TStreamerInfo* _onfileInfo, char const* _elemName)
   {
     TStreamerInfo* currentInfo(static_cast<TStreamerInfo*>(_class->GetStreamerInfo()));
     TObjArrayIter curItr(currentInfo->GetElements());
 
     TStreamerElement* currentElem(0);
-    while(currentElem = static_cast<TStreamerElement*>(curItr()))
+    while((currentElem = static_cast<TStreamerElement*>(curItr())))
       if(std::strcmp(currentElem->GetName(), _elemName) == 0) break;
 
     if(!currentElem)
@@ -26,7 +44,7 @@ namespace mithep {
     TObjArrayIter onfItr(_onfileInfo->GetElements());
 
     TStreamerElement* onfileElem(0);
-    while(onfileElem = static_cast<TStreamerElement*>(onfItr()))
+    while((onfileElem = static_cast<TStreamerElement*>(onfItr())))
       if(std::strcmp(onfileElem->GetName(), _elemName) == 0) break;
 
     if(!onfileElem)
@@ -36,8 +54,28 @@ namespace mithep {
     onfileElem->SetNewType(currentElem->GetNewType());
     onfileElem->SetOffset(currentElem->GetOffset());
     onfileElem->Init();
+  }
 
-    return true;
+  void
+  ConvertStreamer(TClass* _thisClass, Version_t _onfileVersion, void* _this, std::vector<TString> const& _elemNames)
+  {
+    TStreamerInfo* info(static_cast<TStreamerInfo*>(_thisClass->GetStreamerInfos()->UncheckedAt(_onfileVersion)));
+
+    R__LOCKGUARD(gInterpreterMutex);
+
+    if(info->IsCompiled()) return;
+
+    for(unsigned iE(0); iE != _elemNames.size(); ++iE)
+      ReplaceStreamerElement(_thisClass, info, _elemNames[iE]);
+
+    _thisClass->BuildRealData(_this);
+    info->BuildOld();
+  }
+
+  void
+  ConvertStreamer(TClass* _thisClass, Version_t _onfileVersion, void* _this, char const* _elemName)
+  {
+    ConvertStreamer(_thisClass, _onfileVersion, _this, std::vector<TString>{_elemName});
   }
 
 }
