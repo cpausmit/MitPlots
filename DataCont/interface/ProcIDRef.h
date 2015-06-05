ab5384a4b05aa24168587b5cd4f06922748a7be7
Yutaro Iiyama <yiiyama@mit.edu>
2015-06-05 10:16:58 -0400

    adding copy constructors but ended up not needing them

--- DataCont/interface/ProcIDRef.h
+++ DataCont/interface/ProcIDRef.h
@@ -1,6 +1,4 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: ProcIDRef.h,v 1.4 2009/03/02 12:34:00 loizides Exp $
-//
 // ProcIDRef
 //
 // Implementation of a process id that can be stored in the
@@ -17,18 +15,23 @@
 
 namespace mithep 
 {
-  class ProcIDRef : public TObject
-  {
-    public:
-      ProcIDRef() : fProcID(0)                         {}
-      ProcIDRef(TProcessID *pid) : fProcID(pid)        {}
-      ~ProcIDRef()                                     { fProcID = 0;    }
+  class ProcIDRef : public TObject {
+  public:
+    ProcIDRef() : fProcID(0) {}
+    ProcIDRef(ProcIDRef const& orig) : fProcID(orig.fProcID) {}
+    ProcIDRef(TProcessID *pid) : fProcID(pid) {}
+    ~ProcIDRef() {}
 
-      TProcessID        *Pid()                   const { return fProcID; }
-      void               SetPid(TProcessID *pid)       { fProcID = pid;  }
+    ProcIDRef& operator=(ProcIDRef const& rhs) {
+      fProcID = rhs.fProcID;
+      return *this;
+    }
 
-    protected:
-      TProcessID        *fProcID; //!ptr to the Process Unique Identifier
+    TProcessID* Pid() const { return fProcID; }
+    void SetPid(TProcessID* pid) { fProcID = pid;  }
+
+  protected:
+    TProcessID* fProcID; //!ptr to the Process Unique Identifier
 
     ClassDef(ProcIDRef, 1) // Implementation of our own TProcIDRef
   };
