ddbf90e018e8562688364c99c6cd79e98f5db427
Yutaro Iiyama <yiiyama@mit.edu>
2015-06-09 02:35:04 -0400

    execution ordering and printouts

--- bin/process.py
+++ bin/process.py
@@ -12,28 +12,23 @@ argParser.add_argument('analysisCfg', metavar = 'ANALYSIS')
 argParser.add_argument('--cfg', '-c', metavar = 'FILE', dest = 'configFileName')
 argParser.add_argument('--book', '-b', metavar = 'BOOK', dest = 'book')
 argParser.add_argument('--dataset', '-d', metavar = 'DATASET', dest = 'dataset')
-argParser.add_argument('--fileset', '-s', metavar = 'FILESET', dest = 'fileset')
+argParser.add_argument('--filesets', '-s', metavar = 'FILESETS', dest = 'filesets', nargs = '*')
 
 argParser.add_argument('--name', '-n', metavar = 'NAME', dest = 'taskName')
 
-argParser.add_argument('--condor-template', '-t', metavar = 'FILE', dest = 'condorTemplateName', default = os.path.dirname(os.path.realpath(__file__)) + '/condor_template.jdl')
+argParser.add_argument('--condor-template', '-t', metavar = 'FILE', dest = 'condorTemplateName', default = os.environ['CMSSW_BASE'] + '/src/MitAna/config/condor_template.jdl')
 argParser.add_argument('--no-hierarchy', '-a', action = 'store_true', dest = 'anarchy')
 
 args = argParser.parse_args()
 sys.argv = []
 
 if not os.path.exists(args.analysisCfg):
-    raise RuntimeError('Analysis configuration ' + args.analysisCfg + ' does not exist')
+    raise RuntimeError('Analysis configuration file ' + args.analysisCfg + ' does not exist')
 
-taskName = args.taskName
-if not taskName:
-    if args.configFileName:
-        taskName = args.configFileName[:args.configFileName.find('.')]
-    else:
-        taskName = str(int(time.time()))
+if args.configFileName and not os.path.exists(args.configFileName):
+    raise RuntimeError('Task configuration file ' + args.configFileName + ' does not exist')
 
 catalogDirName = os.environ['MIT_CATALOG']
-logDirName = os.environ['MIT_PROD_LOGS'] + '/' + taskName
 
 datasets = []
 
@@ -49,6 +44,10 @@ if args.configFileName:
 elif args.book and args.dataset:
     datasets.append((args.book, args.dataset))
 
+if len(datasets) == 0:
+    print 'No valid dataset found.'
+    sys.exit(1)
+
 filesets = []
 catalogs = []
 
@@ -56,22 +55,58 @@ for book, dataset in datasets:
     with open(catalogDirName + '/' + book + '/' + dataset + '/Filesets') as catalog:
         for line in catalog:
             filesetId = line.strip().split()[0]
-            if args.fileset and filesetId != args.fileset:
+            if len(args.filesets) and filesetId not in args.filesets:
                 continue
 
             filesets.append((book, dataset, filesetId))
 
     catalogs.append(book + '/' + dataset)
 
+if len(filesets) == 0:
+    print 'No valid fileset found.'
+    sys.exit(1)
+
 x509File = '/tmp/x509up_u' + str(os.getuid())
+if not os.path.exists(x509File):
+    print 'x509 proxy missing. You will not be able to download files from T2 in case T3 cache does not exist.'
+    print 'Continue? [y/n]:'
+    while True:
+        response = sys.stdin.readline()
+        if response.strip() == 'y':
+            break
+        elif response.strip() == 'n':
+            sys.exit(0)
+        else:
+            print '[y/n]:'
+            
+    x509File = ''
+
+taskName = args.taskName
+if not taskName:
+    if args.configFileName:
+        taskName = args.configFileName[:args.configFileName.find('.')]
+    else:
+        taskName = str(int(time.time()))
+
+print 'Creating task', taskName
+
+logDirName = os.environ['MIT_PROD_LOGS'] + '/' + taskName
+os.mkdir(logDirName)
 
 taskDirName = os.environ['MIT_PROD_HIST'] + '/' + taskName
+os.mkdir(taskDirName)
 
-envFileName = taskDirName + '/taskenv.py'
+envFileName = taskDirName + '/taskenv.sh'
 with open(envFileName, 'w') as envFile:
-    envFile.write('SCRAM_ARCH = "' + os.environ['SCRAM_ARCH'] + '"\n')
-    envFile.write('CMSSW_RELEASE = "' + os.path.basename(os.environ['CMSSW_RELEASE_BASE']) + '"\n')
-    envFile.write('PYTHONPATH = "' + os.path.basename(os.environ['PYTHONPATH']) + '"\n')
+    envFile.write('export SCRAM_ARCH="' + os.environ['SCRAM_ARCH'] + '"\n')
+    envFile.write('export CMSSW_RELEASE="' + os.path.basename(os.environ['CMSSW_RELEASE_BASE']) + '"\n')
+
+analysisCfgName = taskDirName + '/analysis.py'
+with open(analysisCfgName, 'w') as analysisCfg:
+    with open(args.analysisCfg) as source:
+        for line in source:
+            if not re.search('MitAna.TreeMod.bambu', line.strip()):
+                analysisCfg.write(line)
 
 catalogPackName = taskDirName + '/catalogs.tar.gz'
 proc = subprocess.Popen(['tar', 'czf', catalogPackName, '-C', catalogDirName] + catalogs, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
@@ -86,8 +121,9 @@ libTarballName = os.path.dirname(cmsswbase) + '/libraries.tar.gz'
 remakeTarball = True
 if os.path.exists(libTarballName):
     tarballLastUpdate = os.path.getmtime(libTarballName)
-    for lib in os.listdir(cmsswbase + '/lib/' + os.environ['SCRAM_ARCH']):
-        if os.path.getmtime(lib) > tarballLastUpdate:
+    libDirName = cmsswbase + '/lib/' + os.environ['SCRAM_ARCH']
+    for lib in os.listdir(libDirName):
+        if os.path.getmtime(libDirName) > tarballLastUpdate:
             break
     else:
         remakeTarball = False
@@ -106,18 +142,31 @@ with open(args.condorTemplateName) as condorTemplate:
         if not re.match('#', line.strip()):
             condorStr += line
 
+proc = subprocess.Popen(['condor_q', '-submitter', os.environ['USER'], '-autoformat', 'Iwd', 'Args'], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+out, err = proc.communicate()
+
+running = []
+
+for line in out.split('\n'):
+    matches = re.match('Iwd = (.*) +Args = ([^ ]+) ([^ ]+) ([^ ]+)')
+    running.append((os.path.basename(matches.group(1)), matches.group(2), matches.group(3), matches.group(4)))
+
 for book, dataset, filesetId in filesets:
+    if (taskName, book, dataset, filesetId) in running:
+        continue
+
     jobDirName = taskDirName + '/' + book + '/' + dataset
+
+    try:
+        os.makedirs(jobDirName)
+    except:
+        pass
 
     jdlFileName = jobDirName + '/' + filesetId + '.jdl'
     with open(jdlFileName, 'w') as jdlFile:
         jdlFile.write(condorStr + '\n')
         
-        line = 'Arguments ='
-        line += ' --book=' + book
-        line += ' --dataset=' + dataset
-        line += ' --fileset=' + filesetId
-        line += ' ' + os.path.basename(args.analysisCfg)
+        line = 'Arguments = ' + book + ' ' + dataset + ' ' + filesetId
         jdlFile.write(line + '\n')
 
         line = 'Output = ' + logDirName + '/' + filesetId + '.out'
@@ -129,25 +178,26 @@ for book, dataset, filesetId in filesets:
         line = 'Log = ' + logDirName + '/' + filesetId + '.log'
         jdlFile.write(line + '\n')
 
-        line = 'transfer_input_files ='
-        line += ' ' + x509File + ','
-        line += ' ' + args.analysisCfg + ','
+        line = 'transfer_input_files = run.py,'
+        if x509File:
+            line += ' ' + x509File + ','
+        line += ' ' + analysisCfgName + ','
         line += ' ' + envFileName + ','
         line += ' ' + libTarballName + ','
         line += ' ' + catalogPackName
         jdlFile.write(line + '\n')
 
-        line = 'transfer_output_files ='
-        line += dataset + '_' + fileset + '.root'
+        line = 'transfer_output_files = ' + dataset + '_' + filesetId + '.root'
+        jdlFile.write(line + '\n')
+
+        line = 'Initialdir = ' + jobDirName
         jdlFile.write(line + '\n')
 
         jdlFile.write('Queue\n')
 
-#    proc = subprocess.Popen(['condor_submit', jdlFileName], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-#    out, err = proc.communicate()
-#    print out
-#    if err.strip():
-#        sys.stderr.write(err)
-#        sys.stderr.flush()
-
-    
+    proc = subprocess.Popen(['condor_submit', jdlFileName], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    out, err = proc.communicate()
+    print out
+    if err.strip():
+        sys.stderr.write(err)
+        sys.stderr.flush()
--- bin/run.py
+++ bin/run.py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python
-
 import sys
-import os
-import subprocess
 from argparse import ArgumentParser
 
 argParser = ArgumentParser(description = 'Run BAMBU analysis')
@@ -14,42 +10,8 @@ argParser.add_argument('--fileset', '-s', metavar = 'FILESET', dest = 'fileset')
 args = argParser.parse_args()
 sys.argv = []
 
-workdir = os.path.dirname(os.path.realpath(__file__))
-
-from taskenv import SCRAM_ARCH, CMSSW_RELEASE
-
-os.environ['SCRAM_ARCH'] = SCRAM_ARCH
-
-proc = subprocess.Popen('source /cvmfs/cms.cern.ch/cmsset_default.sh; scram project CMSSW ' + CMSSW_RELEASE, shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-out, err = proc.communicate()
-print out
-if err.strip():
-    sys.stderr.write(err)
-    sys.stderr.flush()
-
-if proc.returncode != 0:
-    sys.exit(proc.returncode)
-
-proc = subprocess.Popen(['tar', 'xzf', 'libraries.tar.gz'], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-out, err = proc.communicate()
-print out
-if err.strip():
-    sys.stderr.write(err)
-    sys.stderr.flush()
-
-os.mkdir('catalog')
-proc = subprocess.Popen(['tar', 'xzf', '-C', 'catalog', 'catalogs.tar.gz'], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-out, err = proc.communicate()
-print out
-if err.strip():
-    sys.stderr.write(err)
-    sys.stderr.flush()
-
-sys.path = os.environ['PYTHONPATH'].split(':')
-
 import ROOT
 ROOT.gROOT.SetBatch(True)
-ROOT.gSystem.AddDynamicPath(CMSSW_RELEASE + '/lib/' + SCRAM_ARCH)
 
 ROOT.gSystem.Load('libMitAnaTreeMod.so')
 
@@ -60,7 +22,7 @@ analysis = mithep.Analysis()
 catalog = mithep.Catalog('catalog')
 dataset = catalog.FindDataset(args.book, args.dataset, args.fileset, 1)
 analysis.AddDataset(dataset)
-analysis.SetOutputName(dataset + '_' + fileset + '.root')
+analysis.SetOutputName(args.dataset + '_' + args.fileset + '.root')
 analysis.SetUseCacher(1)
 analysis.SetKeepHierarchy(True)
 
