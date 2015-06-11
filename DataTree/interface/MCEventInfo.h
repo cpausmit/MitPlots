8ee88fa31239aafb92f3a9a7b4a5fe2dbe024863
Yutaro Iiyama <yiiyama@mit.edu>
2015-06-11 10:35:42 -0400

    updating MC info classes

--- DataTree/interface/MCEventInfo.h
+++ DataTree/interface/MCEventInfo.h
@@ -11,53 +11,133 @@
 #define MITANA_DATATREE_MCEVENTINFO_H
  
 #include "MitAna/DataTree/interface/DataBase.h"
+#include "MitCommon/DataFormats/interface/Types.h"
 
-namespace mithep 
-{
-  class MCEventInfo : public DataBase
-  {
-    public:
-      MCEventInfo() : fProcessId(0), fScale(0), fWeight(0), fId1(0), fId2(0), 
-                      fX1(0), fX2(0), fScalePdf(0), fPdf1(0), fPdf2(0), fFlavorHistoryPath(0) {}
+namespace mithep {
 
-      Int_t               Id1()                      const { return fId1;                 }
-      Int_t               Id2()                      const { return fId2;                 }
-      EObjType            ObjType()                  const { return kMCEventInfo;         }      
-      Double_t            Pdf1()                     const { return fPdf1;                }
-      Double_t            Pdf2()                     const { return fPdf2;                }
-      Int_t               ProcessId()                const { return fProcessId;           }
-      Double_t            Scale()                    const { return fScale;               }
-      Double_t            ScalePdf()                 const { return fScalePdf;            }
-      Double_t            X1()                       const { return fX1;                  }
-      Double_t            X2()                       const { return fX2;                  }
-      Double_t            Weight()                   const { return fWeight;              }
-      Int_t               FlavorHistoryPath()        const { return fFlavorHistoryPath;   }
-      void                SetId1(Int_t id)                    { fId1       = id;          }
-      void                SetId2(Int_t id)                    { fId2       = id;          }
-      void                SetPdf1(Double_t p)                 { fPdf1      = p;           }
-      void                SetPdf2(Double_t p)                 { fPdf2      = p;           }
-      void                SetProcessId(Int_t id)              { fProcessId = id;          }
-      void                SetScale(Double_t s)                { fScale     = s;           }
-      void                SetScalePdf(Double_t s)             { fScalePdf  = s;           }
-      void                SetWeight(Double_t w)               { fWeight    = w;           }
-      void                SetX1(Double_t x)                   { fX1        = x;           }
-      void                SetX2(Double_t x)                   { fX2        = x;           }
-      void                SetFlavorHistoryPath(Int_t p)       { fFlavorHistoryPath = p;   }
+  class MCEventInfo : public DataBase {
+  public:
+    MCEventInfo();
+    ~MCEventInfo();
 
-    protected:
-      Int_t         fProcessId;           //process id
-      Double32_t    fScale;               //scale
-      Double32_t    fWeight;              //weight
-      Int_t         fId1;                 //pdg of first parton
-      Int_t         fId2;                 //pdg of second parton
-      Double32_t    fX1;                  //bjorken x1
-      Double32_t    fX2;                  //bjorken x2
-      Double32_t    fScalePdf;            //scale (Q^2)
-      Double32_t    fPdf1;                //pdf1/x1
-      Double32_t    fPdf2;                //pdf2/x2
-      Int_t         fFlavorHistoryPath;   //Number indicating flavor history
+    Int_t Id1() const { return fId1; }
+    Int_t Id2() const { return fId2; }
+    EObjType ObjType() const { return kMCEventInfo; } 
+    Double_t Pdf1() const { return fPdf1; }
+    Double_t Pdf2() const { return fPdf2; }
+    Int_t ProcessId() const { return fProcessId; }
+    Double_t Scale() const { return fScale; }
+    Double_t ScalePdf() const { return fScalePdf; }
+    Double_t X1() const { return fX1; }
+    Double_t X2() const { return fX2; }
+    Double_t Weight() const { return fWeight; }
+    Int_t FlavorHistoryPath() const { return fFlavorHistoryPath; }
+    Bool_t HasLHEInfo() const { return fHasLHEInfo; }
+    UInt_t NReweightScaleFactors() const { return fReweightScaleFactors.size(); }
+    Double_t ReweightScaleFactor(UInt_t idx) const { return fReweightScaleFactors.at(idx); }
+    UInt_t NScales() const { return fScales.size(); }
+    Double_t Scale(UInt_t idx) const { return fScales.at(idx); }
+    UShort_t NPartonsLO() const { return fNPartonsLO; }
+    UShort_t NPartonsNLO() const { return fNPartonsNLO; }
+    UShort_t NPartons() const { return fNPartons; }
+    Double_t AlphaQED() const { return fAlphaQED; }
+    Double_t AlphaQCD() const { return fAlphaQCD; }
+    Int_t PartonId(UInt_t iP) const { return iP < fNPartons ? fPartonId[iP] : 0; }
+    Short_t PartonStatus(UInt_t iP) const { return iP < fNPartons ? fPartonStatus[iP] : -1; }
+    UShort_t PartonMother1(UInt_t iP) const { return iP < fNPartons ? fPartonMother1[iP] : -1; }
+    UShort_t PartonMother2(UInt_t iP) const { return iP < fNPartons ? fPartonMother2[iP] : -1; }
+    Short_t PartonColor1(UInt_t iP) const { return iP < fNPartons ? fPartonColor1[iP] : 0; }
+    Short_t PartonColor2(UInt_t iP) const { return iP < fNPartons ? fPartonColor2[iP] : 0; }
+    mithep::FourVector const* PartonMom(UInt_t iP) const { return iP < fNPartons ? fPartonMom + iP : 0; }
+    Double_t PartonCTau(UInt_t iP) const { return iP < fNPartons ? fPartonCTau[iP] : -1.; }
+    Double_t PartonSpin(UInt_t iP) const { return iP < fNPartons ? fPartonSpin[iP] : 0.; }
+    UInt_t NLHECommentLines() const { return fLHEComments.size(); }
+    char const* LHEComment(UInt_t iL) const { return fLHEComments.at(iL).Data(); }
 
-    ClassDef(MCEventInfo, 3) // Monte-Carlo event info class
+    // Using HEPEUP speak
+    UShort_t NUP() const { return NPartons(); }
+    Int_t IDPRUP() const { return ProcessId(); }
+    Double_t XWGTUP() const { return Weight(); }
+    Double_t SCALUP() const { return Scale(); }
+    Double_t AQEDUP() const { return AlphaQED(); }
+    Double_t AQCDUP() const { return AlphaQCD(); }
+    Int_t IDUP(UInt_t iP) const { return PartonId(iP); }
+    Short_t ISTUP(UInt_t iP) const { return PartonStatus(iP); }
+    std::pair<UShort_t, UShort_t> MOTHUP(UInt_t iP) const { return std::make_pair(PartonMother1(iP), PartonMother2(iP)); }
+    std::pair<Short_t, Short_t> ICOLUP(UInt_t iP) const { return std::make_pair(PartonColor1(iP), PartonColor2(iP)); }
+    mithep::FourVector const* PUP(UInt_t iP) const { return PartonMom(iP); }
+    Double_t VTIMUP(UInt_t iP) const { return PartonCTau(iP); }
+    Double_t SPINUP(UInt_t iP) const { return PartonSpin(iP); }
+
+    void SetId1(Int_t id) { fId1 = id; }
+    void SetId2(Int_t id) { fId2 = id; }
+    void SetPdf1(Double_t p) { fPdf1 = p; }
+    void SetPdf2(Double_t p) { fPdf2 = p; }
+    void SetProcessId(Int_t id) { fProcessId = id; }
+    void SetScale(Double_t s) { fScale = s; }
+    void SetScalePdf(Double_t s) { fScalePdf = s; }
+    void SetWeight(Double_t w) { fWeight = w; }
+    void SetX1(Double_t x) { fX1 = x; }
+    void SetX2(Double_t x) { fX2 = x; }
+    void SetFlavorHistoryPath(Int_t p) { fFlavorHistoryPath = p; }
+    void SetHasLHEInfo(Bool_t b) { fHasLHEInfo = b; }
+    void SetNReweightScaleFactors(UInt_t size) { fReweightScaleFactors.resize(size); }
+    void SetReweightScaleFactors(UInt_t iF, Double_t s) { fReweightScaleFactors.at(iF) = s; }
+    void ClearScales() { fScales.clear(); }
+    void AddScale(Double_t s) { fScales.push_back(s); }
+    void SetNPartonsLO(UShort_t n) { fNPartonsLO = n; }
+    void SetNPartonsNLO(UShort_t n) { fNPartonsNLO = n; }
+    void SetNPartons(UShort_t);
+    void SetAlphaQED(Double_t a) { fAlphaQED = a; }
+    void SetAlphaQCD(Double_t a) { fAlphaQCD = a; }
+    void SetPartonId(UShort_t iP, Int_t id) { fPartonId[iP] = id; }
+    void SetPartonStatus(UShort_t iP, Short_t s) { fPartonStatus[iP] = s; }
+    void SetPartonMother1(UShort_t iP, UShort_t m) { fPartonMother1[iP] = m; }
+    void SetPartonMother2(UShort_t iP, UShort_t m) { fPartonMother2[iP] = m; } 
+    void SetPartonColor1(UShort_t iP, Short_t c) { fPartonColor1[iP] = c; }
+    void SetPartonColor2(UShort_t iP, Short_t c) { fPartonColor2[iP] = c; }
+    void SetPartonMom(UShort_t iP, Double_t x, Double_t y, Double_t z, Double_t e)
+    { fPartonMom[iP].SetPxPyPzE(x, y, z, e); }
+    void SetPartonCTau(UShort_t iP, Double_t c) { fPartonCTau[iP] = c; }
+    void SetPartonSpin(UShort_t iP, Double_t s) { fPartonSpin[iP] = s; }
+    void ClearLHEComments() { fLHEComments.clear(); }
+    void AddLHEComment(char const* c) { fLHEComments.push_back(c); }
+
+  protected:
+    Int_t fProcessId; //process id
+    Double32_t fScale; //scale
+    Double32_t fWeight; //weight
+    Int_t fId1; //pdg of first parton
+    Int_t fId2; //pdg of second parton
+    Double32_t fX1; //bjorken x1
+    Double32_t fX2; //bjorken x2
+    Double32_t fScalePdf; //scale (Q^2)
+    Double32_t fPdf1; //pdf1/x1
+    Double32_t fPdf2; //pdf2/x2
+    Int_t fFlavorHistoryPath; //Number indicating flavor history
+    // all data below are from LHE
+    Bool_t fHasLHEInfo;
+    std::vector<Double32_t> fReweightScaleFactors; // alternative event weights normalized to original XWGTUP
+    std::vector<Double32_t> fScales; // scale value used to exclude EWK-produced partons from matching
+    UShort_t fNPartonsLO; // number of partons for LO process (used to steer matching/merging)
+    UShort_t fNPartonsNLO; // number of partons for NLO process (used to steer matching/merging)
+    UShort_t fNPartons; // number of partons excluding the first two
+    UShort_t fPartonsSize; //! only for bookkeeping array size
+    Double32_t fAlphaQED; // value of QED coupling used in the event
+    Double32_t fAlphaQCD; // value of QCD coupling used in the event
+    Int_t* fPartonId; //[fNPartons] pdg id of the partons
+    Short_t* fPartonStatus; //[fNPartons] status of the partons
+    UShort_t* fPartonMother1; //[fNPartons] mother 1 of the partons
+    UShort_t* fPartonMother2; //[fNPartons] mother 2 of the partons
+    Short_t* fPartonColor1; //[fNPartons] color line 1 of the partons
+    Short_t* fPartonColor2; //[fNPartons] color line 1 of the partons
+    mithep::FourVector* fPartonMom; //[fNPartons] momentum of the partons
+    Double32_t* fPartonCTau; //[fNPartons] ctau of the partons
+    Double32_t* fPartonSpin; //[fNPartons] spin of the partons
+    std::vector<TString> fLHEComments;
+
+    ClassDef(MCEventInfo, 4) // Monte-Carlo event info class
   };
+
 }
 #endif
--- DataTree/interface/MCRunInfo.h
+++ DataTree/interface/MCRunInfo.h
@@ -1,7 +1,7 @@
 //--------------------------------------------------------------------------------------------------
 // MCRunInfo
 //
-// This class holds LHE reweighting definitions. It is stored in the run tree.
+// This class holds run-wide MC information. It is stored in the run tree.
 //
 // Authors: Y.Iiyama
 //--------------------------------------------------------------------------------------------------
@@ -20,7 +20,22 @@ namespace mithep {
 
     MCRunInfo();
     virtual ~MCRunInfo();
+
+    EObjType ObjType() const override { return kMCRunInfo; }
     
+    Bool_t HasLHEInfo() const { return fHasLHEInfo; }
+    Double_t BeamEnergy() const { return fBeamEnergy; }
+    Int_t PdfGroup() const { return fPdfGroup; }
+    Int_t PdfId() const { return fPdfId; }
+    Int_t WeightMode() const { return fWeightMode; }
+    UInt_t NProcesses() const { return fNProcesses; }
+    Double_t ProcessXSec(UInt_t iP) const { return iP < fNProcesses ? fProcessXSec[iP] : -1.; }
+    Double_t ProcessXSecError(UInt_t iP) const { return iP < fNProcesses ? fProcessXSecError[iP] : 0.; }
+    Double_t ProcessXSecMax(UInt_t iP) const { return iP < fNProcesses ? fProcessXSecMax[iP] : 0.; }
+    Int_t ProcessId(UInt_t iP) const { return iP < fNProcesses ? fProcessId[iP] : -1; }
+    UInt_t NHeaderBlocks() const { return fNHeaderBlocks; }
+    char const* HeaderBlockTag(UInt_t iB) { return iB < fNHeaderBlocks ? fHeaderBlockTag[iB].Data() : ""; }
+    char const* HeaderBlockContent(UInt_t iB) { return iB < fNHeaderBlocks ? fHeaderBlockContent[iB].Data() : ""; }
     UInt_t NWeightGroups() const { return fNWeightGroups; }
     char const* WeightGroupCombination(UInt_t iG) const { return iG < fNWeightGroups ? fWeightGroupCombination[iG].Data() : ""; }
     char const* WeightGroupType(UInt_t iG) const { return iG < fNWeightGroups ? fWeightGroupType[iG].Data() : ""; }
@@ -28,19 +43,66 @@ namespace mithep {
     char const* WeightId(UInt_t iG, UInt_t iW) const { return iG < fNWeightGroups ? fWeightData[iG].at(iW).first : ""; };
     char const* WeightData(UInt_t iG, UInt_t iW) const { return iG < fNWeightGroups ? fWeightData[iG].at(iW).second : ""; }
     char const* WeightData(UInt_t, char const*) const;
+    UShort_t WeightPositionInEvent(UInt_t iG, UInt_t iW) const { return iG < fNWeightGroups ? fWeightPositionInEvent[iG].at(iW) : -1; }
+    UInt_t NLHECommentLines() const { return fLHEComments.size(); }
+    char const* LHEComment(UInt_t iL) const { return fLHEComments.at(iL).Data(); }
 
+    // Using HEPRUP speak
+    Double_t EBMUP() const { return BeamEnergy(); }
+    Int_t PDFGUP() const { return PdfGroup(); }
+    Int_t PDFSUP() const { return PdfId(); }
+    Int_t IdWTUP() const { return WeightMode(); }
+    UInt_t NPRUP() const { return NProcesses(); }
+    Double_t XSECUP(UInt_t iP) const { return ProcessXSec(iP); }
+    Double_t XERRUP(UInt_t iP) const { return ProcessXSecError(iP); }
+    Double_t XMAXUP(UInt_t iP) const { return ProcessXSecMax(iP); }
+    Int_t LPRUP(UInt_t iP) const { return ProcessId(iP); }
+
+    void SetHasLHEInfo(Bool_t f) { fHasLHEInfo = f; }
+    void SetBeamEnergy(Double_t e) { fBeamEnergy = e; }
+    void SetPdfGroup(Int_t g) { fPdfGroup = g; }
+    void SetPdfId(Int_t i) { fPdfId = i; }
+    void SetWeightMode(Int_t m) { fWeightMode = m; }
+    void SetNProcesses(UInt_t);
+    void SetProcessXSec(UInt_t iP, Double_t v) { fProcessXSec[iP] = v; }
+    void SetProcessXSecError(UInt_t iP, Double_t v) { fProcessXSecError[iP] = v; }
+    void SetProcessXSecMax(UInt_t iP, Double_t v) { fProcessXSecMax[iP] = v; }
+    void SetProcessId(UInt_t iP, Int_t v) { fProcessId[iP] = v; }
+    void SetNHeaderBlocks(UInt_t);
+    void SetHeaderBlockTag(UInt_t iB, char const* s) { fHeaderBlockTag[iB] = s; }
+    void SetHeaderBlockContent(UInt_t iB, char const* s) { fHeaderBlockContent[iB] = s; }
+    void ClearWeightData();
     void SetNWeightGroups(UInt_t);
     void SetWeightGroupCombination(UInt_t iG, char const* s) { fWeightGroupCombination[iG] = s; }
     void SetWeightGroupType(UInt_t iG, char const* s) { fWeightGroupType[iG] = s; }
-    void AddWeightData(UInt_t iG, char const* id, char const* d) { fWeightData[iG].push_back(IdValuePair(id, d)); }
-    void ClearWeightData();
+    void AddWeightData(UInt_t iG, char const* id, char const* d, UShort_t idx)
+    { fWeightData[iG].push_back(IdValuePair(id, d)); fWeightPositionInEvent[iG].push_back(idx); }
+    void ClearLHEComments() { fLHEComments.clear(); }
+    void AddLHECommentLine(TString const& c) { fLHEComments.push_back(c); }
 
   protected:
+    Bool_t fHasLHEInfo; // Everything below comes from LHE
+    Double_t fBeamEnergy;
+    Int_t fPdfGroup;
+    Int_t fPdfId;
+    Int_t fWeightMode;
+    UInt_t fNProcesses;
+    UInt_t fProcessesSize; //! to keep track of the array size
+    Double_t* fProcessXSec; //[fNProcesses]
+    Double_t* fProcessXSecError; //[fNProcesses]
+    Double_t* fProcessXSecMax; //[fNProcesses]
+    Int_t* fProcessId; //[fNProcesses]
+    UInt_t fNHeaderBlocks;
+    UInt_t fHeaderBlocksSize; //! to keep track of the array size
+    TString* fHeaderBlockTag; //[fNHeaderBlocks]
+    TString* fHeaderBlockContent; //[fNHeaderBlocks]
     UInt_t fNWeightGroups;
     UInt_t fWeightGroupsSize; //! to keep track of the array size
     TString* fWeightGroupCombination; //[fNWeightGroups]
     TString* fWeightGroupType; //[fNWeightGroups]
     IdValuePairVector* fWeightData; //[fNWeightGroups]
+    std::vector<UShort_t>* fWeightPositionInEvent; //[fNWeightGroups] position of the reweighting factor in the events
+    std::vector<TString> fLHEComments;
 
     ClassDef(MCRunInfo, 1) // MC Run info class
   };
