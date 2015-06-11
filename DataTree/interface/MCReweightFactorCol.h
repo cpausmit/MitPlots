f24062bb6e4893f3435d3de1ae725b5e6f0fffe9
Yutaro Iiyama <yiiyama@mit.edu>
2015-06-11 10:36:37 -0400

    new class MCReweightFactor

---
+++ DataTree/interface/MCReweightFactorCol.h
@@ -0,0 +1,9 @@
+#ifndef MITANA_DATATREE_MCREWEIGHTFACTORCOL_H
+#define MITANA_DATATREE_MCREWEIGHTFACTORCOL_H
+
+#include "MitAna/DataCont/interface/Collection.h"
+#include "MitAna/DataCont/interface/Array.h"
+#include "MitAna/DataCont/interface/ObjArray.h"
+#include "MitAna/DataTree/interface/MCReweightFactorFwd.h"
+#include "MitAna/DataTree/interface/MCReweightFactor.h"
+#endif
