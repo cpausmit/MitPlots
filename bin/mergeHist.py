3cc802ca06bf00fc30e54f1ee4f04efe1a220963
Yutaro Iiyama <yiiyama@mit.edu>
2015-06-12 08:26:07 -0400

    automatic output file system check + using modern tools

--- bin/mergeHist.py
+++ bin/mergeHist.py
@@ -4,137 +4,139 @@
 # Merge root files from a given datasets together.
 #
 #===================================================================================================
-import sys, getopt, os, re, fnmatch, commands
-import string
-
-#===================================================================================================
-def usage():
-    print "\n" \
-          "possible options are:                 \n" \
-          "    --help                            \n" \
-          "    --debug                           \n" \
-          "    --Dataset=<dataset>               \n" \
-          "    --Skim=< noskim | skim >          \n" \
-          "    --InputPath=<myInputPath>         \n" \
-          "    --OutputPath=<myOutputPath>       \n" \
-          "    --FilenameHeader=<myFileHeader>   \n"
-
-#===================================================================================================
-def filesExist(path,filenameExpression):
-    exists = False
-    
-    if not os.path.exists(path):
-      return exists;
-    
-    for fileName in os.listdir(path):
-        if fnmatch.fnmatch (fileName,filenameExpression):
-            exists = True
-    return exists
+import sys, os, re
+import time
+import subprocess
+import shutil
 
 #===================================================================================================
 # Merge Filesets
 #===================================================================================================
-def MergeFilesets(debug,dataset,skim,inputPath,outputPath,filenameHeader):
+def MergeFilesets(inputPath, outputPath, filenameHeader, datasets = [], skim = '', overwrite = False, debug = False):
 
-    # say what we try to do
-    print " Merging files for dataset: " + dataset + " (skim: " + skim + ")"
+    proc = subprocess.Popen(['which', 'hadd'], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    out, err = proc.communicate()
+    if proc.returncode != 0:
+        print "Cannot use merge command `hadd'. Please make sure ROOT environment is set."
+        sys.exit(1)
 
     # create the output directory in case it is not yet there
-    cmd              = 'mkdir -p ' + outputPath
-    os.system(cmd)
+    outputPath = os.path.realpath(outputPath)
+    if not os.path.exists(outputPath):
+        os.makedirs(outputPath)
 
-    # output name
-    outputMergedFilename = filenameHeader + '_' + dataset + '_' + skim + '.root'
+    # check if the output path is in the local file system
+    # if not, will create a temp path in /tmp and copy the final product over
+    local = True
+    for line in open('/proc/mounts'):
+        fs = line.strip().split()[0]
+        mount = line.strip().split()[1]
+        if outputPath.startswith(mount):
+            if not fs.startswith('/dev'):
+                local = False
+            
+    if local:
+        writePath = outputPath
+    else:
+        try:
+            writePath = '/tmp/' + str(int(time.time()))
+            os.makedirs(writePath)
+        except OSError:
+            time.sleep(1)
 
-    # find all files that have some contents
-    inputFileString = ""
-    files = os.listdir(inputPath+'/'+dataset)
-    for file in files:
-        if not re.search('root',file):
+    # if no datasets were given, pick up all that are in the input directory
+    if len(datasets) == 0:
+        for dirName in os.listdir(inputPath):
+            if os.path.isdir(inputPath + '/' + dirName):
+                datasets.append(dirName)
+
+    # merge input for each dataset
+    for dataset in datasets:
+        # output name
+        if skim:
+            outputMergedFilename = writePath + '/' + filenameHeader + '_' + dataset + '_' + skim + '.root'
+        else:
+            outputMergedFilename = writePath + '/' + filenameHeader + '_' + dataset + '.root'
+
+        if os.path.exists(outputMergedFilename):
+            delete = False
+            if overwrite:
+                print " Overwriting existing output "
+                print " " + outputMergedFilename
+                delete = True
+            else:
+                print " Output file "
+                print " " + outputMergedFilename
+                print " already exists. Do you wish to overwrite? [y/N]:"
+                while True:
+                    response = sys.stdin.readline()
+                    if response.strip() == 'y':
+                        delete = True
+                    elif response.strip() == 'N':
+                        break
+                    else:
+                        print '[y/N]:'
+                        continue
+
+                if delete:
+                    os.remove(outputMergedFilename)
+                else:
+                    print 'Skipping dataset ' + dataset
+                    continue
+    
+        # find all files that have some contents
+        inputPaths = []
+        inputFiles = []
+        for fileName in os.listdir(inputPath + '/' + dataset):
+            if not fileName.endswith('.root'):
+                continue
+    
+            fullPath = inputPath + '/' + dataset + '/' + fileName
+            if os.path.getsize(inputPath + '/' + dataset + '/' + fileName) > 4:
+                inputPaths.append(fullPath)
+                inputFiles.append(fileName)
+            else:
+                print ' WARNING -- empty root file skipped: ' + fullPath
+
+        print 'Merging files from dataset ' + dataset
+        if debug:
+            print ' Files:'
+            print '  ' + ' '.join(sorted(inputFiles))
+
+        proc = subprocess.Popen(['hadd', '-f7', outputMergedFilename] + inputPaths, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+        out, err = proc.communicate()
+
+        if debug and out.strip():
+            print out
+        if err.strip():
+            sys.stderr.write(err)
+            sys.stderr.flush()
+
+        if not os.path.exists(outputMergedFilename):
+            print 'Failed to create ' + outputMergedFilename
             continue
-        if os.path.getsize(inputPath+'/'+dataset+'/'+file) > 4:
-            inputFileString = inputFileString + ' ' + inputPath+'/'+dataset+'/'+file
-        else:
-            print ' WARNING -- empty root file skipped: ' + inputPath+'/'+dataset+'/'+file
 
-    cmd              = 'hadd -f7 ' + outputPath + '/' + outputMergedFilename \
-                       + inputFileString + ' >& ./merging.tmp'
-        
-    if (debug):
-        print " CMD: " + cmd
+        if not local:
+            shutil.copy(outputMergedFilename, outputMergedFilename.replace(writePath, outputPath, 1))
 
-    if (os.path.exists(outputPath+'/'+outputMergedFilename)):
-        print " Warning: merged file already exists. It will be deleted.\n " + \
-              outputPath+'/'+outputMergedFilename
-        os.system('rm ' + outputPath+'/'+outputMergedFilename)
+if __name__ == '__main__':
+    #===================================================================================================
+    # Main Program
+    #===================================================================================================
+    from argparse import ArgumentParser
+    
+    argParser = ArgumentParser(description = 'Merge output files of given datasets')
+    argParser.add_argument('--InputPath', '-i', metavar = 'PATH', dest = 'inputPath', required = True, help = 'Path of dataset directories')
+    argParser.add_argument('--OutputPath', '-o', metavar = 'PATH', dest = 'outputPath', required = True, help = 'Output directory')
+    argParser.add_argument('--Dataset', '-d', metavar = 'DATASET', dest = 'dataset', nargs = '+', default = [], help = 'Space-delimited list of datasets')
+    argParser.add_argument('--Skim', '-s', metavar = '<skim|noskim>', dest = 'skim', nargs = '?', default = '', help = 'Skim suffix if present')
+    argParser.add_argument('--FilenameHeader', '-f', metavar = 'PREFIX', dest = 'filenameHeader', required = True, help = 'Prefix of the output files')
 
-    os.system(cmd)
-
-#===================================================================================================
-# Main Program
-#===================================================================================================
-debug           = False
-datasetListFile = ''
-dataset         = ''
-skim            = ''
-inputPath       = ''
-outputPath      = ''
-filenameHeader  = ''
-versionList     = list()
-datasetNameList = list()
-skimNameList    = list()
-
-if len(sys.argv[1:]) < 1:
-    print "Error: not enough parameters specified"
-    usage()
-    sys.exit()
-
-try:
-    opts, args = getopt.getopt(sys.argv[1:], "hi:o:f:d:s:",
-                               ["InputPath=","OutputPath=","FilenameHeader=","Dataset=","Skim=",
-                                "help","debug"])
-    for o, a in opts:
-        if o in   ("-h", "--help"):
-            usage()
-            sys.exit()
-        if o in   ("--debug"):
-            debug = True
-        elif o in ("-i", "--InputPath"):
-            inputPath = a # + "/"
-        elif o in ("-o", "--OutputPath"):
-            outputPath = a # + "/"
-        elif o in ("-f", "--FilenameHeader"):
-            filenameHeader = a
-        elif o in ("-d", "--Dataset"):
-            dataset = a
-        elif o in ("-s", "--Skim"):
-            skim = a
-        else:
-            usage()
-            sys.exit()
-except getopt.GetoptError:
-    usage()
-    sys.exit(2)
-
-if (inputPath == ''):
-    print "Error: No InputPath specified."
-    sys.exit()
-
-if (outputPath == ''):
-    print "Error: No OutputPath specified."
-    sys.exit()
-
-if (filenameHeader == ''):
-    print "Error: No FilenameHeader specified."
-    sys.exit()
-
-if (dataset == ''):
-    print "Error: No Dataset specified."
-    sys.exit()
-
-if (skim == ''):
-    print "Error: No Skim variable specified."
-    sys.exit()
-
-# Here is where it all happens
-MergeFilesets(debug,dataset,skim,inputPath,outputPath,filenameHeader)
+    argParser.add_argument('--overwrite', '-R', action = 'store_true', dest = 'overwrite', help = 'Overwrite existing output files')
+    argParser.add_argument('--debug', '-D', action = 'store_true', dest = 'debug')
+    
+    args = argParser.parse_args()
+    sys.argv = []
+    
+    # Here is where it all happens
+    MergeFilesets(args.inputPath, args.outputPath, args.filenameHeader, args.dataset, skim = args.skim, overwrite = args.overwrite, debug = args.debug)
