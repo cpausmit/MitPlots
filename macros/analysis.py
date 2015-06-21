c57c6e309b96694ea80f5834b3f1f95d23a8e77a
Yutaro Iiyama <yiiyama@mit.edu>
2015-06-21 13:32:20 -0400

    importing macro file in a contained namespace

--- macros/analysis.py
+++ macros/analysis.py
@@ -1,78 +1,61 @@
-from argparse import ArgumentParser
-import sys
-import pickle
-import os
+def setSequence(sequenceFile):
+    execfile(sequenceFile)
 
-argParser = ArgumentParser(description = 'Run BAMBU analysis')
-argParser.add_argument('config', metavar = 'CONFIG')
-argParser.add_argument('--book', '-b', metavar = 'BOOK', dest = 'book', default = 't2mit/filefi/040')
-argParser.add_argument('--dataset', '-d', metavar = 'DATASET', dest = 'dataset')
-argParser.add_argument('--fileset', '-s', metavar = 'FILESET', dest = 'fileset')
-argParser.add_argument('--file', '-f', metavar = 'INPUT', dest = 'inputFile')
-argParser.add_argument('--output', '-o', metavar = 'FILENAME', dest = 'outputFile')
-argParser.add_argument('--nentries', '-n', metavar = 'N', dest = 'nentries', type = int, default = 1)
-argParser.add_argument('--cacher', '-c', action = 'store_true', dest = 'useCacher')
-argParser.add_argument('--no-hierarchy', '-H', action = 'store_true', dest = 'anarchy')
-argParser.add_argument('--pickle', '-P', action = 'store_true', dest = 'pickleInput')
-argParser.add_argument('--libs', '-L', metavar = 'LIST', dest = 'libListName')
 
-args = argParser.parse_args()
-sys.argv = []
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    import sys
+    import os
+    
+    argParser = ArgumentParser(description = 'Run BAMBU analysis')
+    argParser.add_argument('config', metavar = 'CONFIG')
+    argParser.add_argument('--book', '-b', metavar = 'BOOK', dest = 'book', default = 't2mit/filefi/040')
+    argParser.add_argument('--dataset', '-d', metavar = 'DATASET', dest = 'dataset')
+    argParser.add_argument('--fileset', '-s', metavar = 'FILESET', dest = 'fileset')
+    argParser.add_argument('--file', '-f', metavar = 'INPUT', dest = 'inputFile')
+    argParser.add_argument('--output', '-o', metavar = 'FILENAME', dest = 'outputFile')
+    argParser.add_argument('--nentries', '-n', metavar = 'N', dest = 'nentries', type = int, default = 1)
+    argParser.add_argument('--cacher', '-c', action = 'store_true', dest = 'useCacher')
+    argParser.add_argument('--no-hierarchy', '-H', action = 'store_true', dest = 'anarchy')
+    argParser.add_argument('--libs', '-L', metavar = 'LIST', dest = 'libListName')
+    
+    args = argParser.parse_args()
+    sys.argv = []
+    
+    if args.dataset or args.fileset:
+        if args.book == '' or args.dataset == '' or args.fileset == '':
+            raise RuntimeError('book, dataset, and fileset must be set together.')
+    
+    if args.inputFile and (args.dataset or args.fileset):
+        raise RuntimeError('cannot specify file and dataset/fileset at the same time.')
+    
+    setSequence(args.config)
 
-if args.dataset or args.fileset:
-    if args.book == '' or args.dataset == '' or args.fileset == '':
-        raise RuntimeError('book, dataset, and fileset must be set together.')
-
-if args.inputFile and (args.dataset or args.fileset):
-    raise RuntimeError('cannot specify file and dataset/fileset at the same time.')
-
-if args.pickleInput:
-    import ROOT
-    if args.libListName:
-        with open(args.libListName) as libList:
-            for line in libList:
-                ROOT.gSystem.Load(line.strip())
-
-    else:
-        ROOT.gSystem.Load('libMitAnaTreeMod.so')
-
-    with open(args.config) as cfg:
-        mithep, analysis, superMods = pickle.load(cfg)
-
-    for superMod in superMods:
-        analysis.AddSuperModule(superMod)
-
-else:
     from MitAna.TreeMod.bambu import mithep, analysis
-
-    execfile(args.config)
-
-if args.dataset:
-    catalog = mithep.Catalog('catalog')
-    dataset = catalog.FindDataset(args.book, args.dataset, args.fileset, 1)
-    analysis.AddDataset(dataset)
-    analysis.SetUseCacher(1)
-
-elif args.inputFile:
-    analysis.AddFile(args.inputFile)
-
-if args.outputFile:
-    analysis.SetOutputName(args.outputFile)
-
-if args.nentries > 0:
-    analysis.SetProcessNEvents(args.nentries)
-
-if args.nentries > 0:
-    analysis.SetProcessNEvents(args.nentries)
-
-if args.useCacher:
-    analysis.SetUseCacher(1)
-
-if args.anarchy:
-    analysis.SetKeepHierarchy(False)
-
-print '\n+++++ ANALYSIS FLOW +++++\n'
-analysis.PrintModuleTree()
-print '\n+++++++++++++++++++++++++\n'
-
-analysis.Run(False)
+    
+    if args.dataset:
+        catalog = mithep.Catalog('catalog')
+        dataset = catalog.FindDataset(args.book, args.dataset, args.fileset, 1)
+        analysis.AddDataset(dataset)
+        analysis.SetUseCacher(1)
+    
+    elif args.inputFile:
+        analysis.AddFile(args.inputFile)
+    
+    if args.outputFile:
+        analysis.SetOutputName(args.outputFile)
+    
+    if args.nentries > 0:
+        analysis.SetProcessNEvents(args.nentries)
+    
+    if args.useCacher:
+        analysis.SetUseCacher(1)
+    
+    if args.anarchy:
+        analysis.SetKeepHierarchy(False)
+    
+    print '\n+++++ ANALYSIS FLOW +++++\n'
+    analysis.PrintModuleTree()
+    print '\n+++++++++++++++++++++++++\n'
+    
+    analysis.Run(False)
