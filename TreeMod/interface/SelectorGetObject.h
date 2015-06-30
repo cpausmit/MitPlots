31dc5bb5ce730bfbbd8fd984df515f28e017568c
Yutaro Iiyama <yiiyama@mit.edu>
2015-06-30 09:37:35 -0400

    removing obsolete file

--- TreeMod/interface/SelectorGetObject.h
+++
@@ -1,9 +0,0 @@
-#ifndef MITANA_TREEMOD_SELECTOR_GETOBJECT
-#define MITANA_TREEMOD_SELECTOR_GETOBJECT
-
-#include "MitAna/DataCont/interface/Collection.h"
-#include "MitAna/DataCont/interface/ObjArray.h"
-
-
-
-#endif
